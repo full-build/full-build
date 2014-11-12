a08a3a21b94e80c16f7936b59fe812c674bf76fa
Pierre Chalamet <pierre@chalamet.net>
2014-11-12 22:41:27 +0100

    comment

---
+++ tests/doall.ps1
@@ -0,0 +1,31 @@
+﻿taskkill /im tgitcache.exe
+robocopy titi toto /MIR
+
+try
+{
+    push-location toto
+
+    FullBuild index workspace
+    copy ..\Template.csproj .full-build
+    FullBuild convert projects
+    FullBuild init view cs with cassandra-sharp cassandra-sharp-contrib
+    FullBuild generate view cs
+    FullBuild build view cs
+    FullBuild exec "echo %FULLBUILD_REPO% & git log -n 1 | find ""commit"" && echo."
+    FullBuild exec "git status"
+
+    write-host *********************************************
+    write-host SUCCESS
+    write-host *********************************************
+}
+catch
+{
+
+    write-host *********************************************
+    write-host FAILURE
+    write-host *********************************************
+}
+finally
+{
+    Pop-Location
+}
