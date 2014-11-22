953f05d1b86dafb42d5c2174713e5ebc3cd7d2d4
Pierre Chalamet <pierre@chalamet.net>
2014-11-22 16:46:30 +0100

    fix perpetual build

--- push-release.bat
+++ push-release.bat
@@ -1,5 +1,3 @@
-rem if "%APPVEYOR_REPO_TAG%" NEQ "True" goto :ok
-
 set HERE=%~dp0
 
 echo HERE %HERE%
@@ -7,24 +5,23 @@ echo APPVEYOR_PROJECT_NAME %APPVEYOR_PROJECT_NAME%
 echo CONFIGURATION %CONFIGURATION% 
 echo PLATFORM %PLATFORM% 
 echo APPVEYOR_BUILD_VERSION %APPVEYOR_BUILD_VERSION%
-echo APPVEYOR_REPO_BRANCH %APPVEYOR_REPO_BRANCH%
 echo APPVEYOR_REPO_COMMIT %APPVEYOR_REPO_COMMIT%
 
-7z a %CONFIGURATION%.zip .\src\bin\%CONFIGURATION%\*
+7z a %HERE%%CONFIGURATION%.zip %HERE%\src\bin\%CONFIGURATION%\*
 
 %HERE%tools/github-release.exe release ^
                          --user pchalamet ^
                          --repo full-build ^
 						 --tag %APPVEYOR_BUILD_VERSION% ^
 						 --name "full-build %APPVEYOR_BUILD_VERSION%" ^
-                         --description "%APPVEYOR_PROJECT_NAME% %APPVEYOR_BUILD_VERSION% %PLATFORM% %CONFIGURATION% %APPVEYOR_REPO_COMMIT%" ^
+                         --description "%APPVEYOR_PROJECT_NAME% %CONFIGURATION%^|%PLATFORM% %APPVEYOR_BUILD_VERSION% %APPVEYOR_REPO_COMMIT%" ^
                          --pre-release || goto :ko
 
 %HERE%tools/github-release.exe upload ^
                      --user pchalamet ^
 					 --repo full-build ^
                      --tag %APPVEYOR_BUILD_VERSION% ^
-                     --name "%APPVEYOR_PROJECT_NAME%-net45-%PLATFORM%" ^
+                     --name "%APPVEYOR_PROJECT_NAME%-net45-%PLATFORM%.zip" ^
                      --file %HERE%%CONFIGURATION%.zip || goto :ko
 
 :ok
