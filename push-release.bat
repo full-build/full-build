9bef16e307f7d419c4e54b88b44909b6fea7130e
Pierre Chalamet <pierre@chalamet.net>
2014-11-22 11:00:36 +0100

    repo name

--- push-release.bat
+++ push-release.bat
@@ -11,16 +11,16 @@ git push --tags https://%GITHUB_TOKEN%@github.com/pchalamet/full-build.git 1>NUL
 echo pushing %APPVEYOR_PROJECT_NAME% %CONFIGURATION% %PLATFORM% %APPVEYOR_BUILD_VERSION% from %HERE%
 
 %HERE%tools\github-release.exe release ^
-                         --user pchalamet ^
-                         --repo full-build ^
+                         --user %APPVEYOR_REPO_COMMIT_AUTHOR% ^
+                         --repo %APPVEYOR_PROJECT_NAME% ^
 						 --tag %APPVEYOR_BUILD_VERSION% ^
 						 --name "full-build %APPVEYOR_BUILD_VERSION%" ^
                          --description "%APPVEYOR_PROJECT_NAME% %APPVEYOR_BUILD_VERSION% (%APPVEYOR_REPO_COMMIT%)- %PLATFORM% %CONFIGURATION%" ^
                          --pre-release || goto :ko
 
 %HERE%tools\github-release upload ^
-                     --user pchalamet ^
-					 --repo full-build ^
+                     --user %APPVEYOR_REPO_COMMIT_AUTHOR% ^
+					 --repo %APPVEYOR_PROJECT_NAME% ^
                      --tag %APPVEYOR_BUILD_VERSION% ^
                      --name "%APPVEYOR_PROJECT_NAME%-net45-%PLATFORM%" ^
                      --file %HERE%/src/bin/%CONFIGURATION%.zip || goto :ko
