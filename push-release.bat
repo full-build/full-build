f08f1477af7ec6136e59fde4377a4965cba2a885
Pierre Chalamet <pierre@chalamet.net>
2014-11-22 11:57:02 +0100

    invalid path

--- push-release.bat
+++ push-release.bat
@@ -10,7 +10,7 @@ rem git push --tags https://%GITHUB_TOKEN%@github.com/pchalamet/full-build.git 1
 
 echo pushing %APPVEYOR_PROJECT_NAME% %CONFIGURATION% %PLATFORM% %APPVEYOR_BUILD_VERSION% from %HERE%
 
-%HERE%tools\github-release.exe release ^
+tools/github-release.exe release ^
                          --user %APPVEYOR_REPO_COMMIT_AUTHOR% ^
                          --repo %APPVEYOR_PROJECT_NAME% ^
 						 --tag %APPVEYOR_BUILD_VERSION% ^
@@ -18,12 +18,12 @@ echo pushing %APPVEYOR_PROJECT_NAME% %CONFIGURATION% %PLATFORM% %APPVEYOR_BUILD_
                          --description "%APPVEYOR_PROJECT_NAME% %APPVEYOR_BUILD_VERSION% (%APPVEYOR_REPO_COMMIT%)- %PLATFORM% %CONFIGURATION%" ^
                          --pre-release || goto :ko
 
-%HERE%tools\github-release upload ^
+tools/github-release.exe upload ^
                      --user %APPVEYOR_REPO_COMMIT_AUTHOR% ^
 					 --repo %APPVEYOR_PROJECT_NAME% ^
                      --tag %APPVEYOR_BUILD_VERSION% ^
                      --name "%APPVEYOR_PROJECT_NAME%-net45-%PLATFORM%" ^
-                     --file %HERE%/src/bin/%CONFIGURATION%.zip || goto :ko
+                     --file src/bin/%CONFIGURATION%.zip || goto :ko
 
 :ok
 exit /b 0
