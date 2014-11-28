222aa53fc36861bcac68cc9cb437a7bd27d80d95
cboudereau <cboudereau@gmail.com>
2014-11-28 22:22:08 +0100

    remove unsufull code

--- src/FullBuild/Commands/HostedPackage.cs
+++ src/FullBuild/Commands/HostedPackage.cs
@@ -1,15 +1,25 @@
 using System;
+using System.Linq;
+using System.Xml.Linq;
+using FullBuild.Helpers;
 using FullBuild.Model;
 
 namespace FullBuild.Commands
 {
     internal class HostedPackage : Package
     {
-        public static HostedPackage CreateFrom(Package package, NuGetResult nuGetResult)
+        public static HostedPackage CreateFrom(Package package, XContainer entry)
         {
-            return new HostedPackage(package.Name, nuGetResult.Version, new Uri(nuGetResult.Content), nuGetResult.PackageSize, nuGetResult.PackageHash, nuGetResult.IsLatestVersion, nuGetResult.Published);
+            var content = entry.Descendants(XmlHelpers.Atom + "content").Single().Attribute("src").Value;
+            var packageSize = Int64.Parse(entry.Descendants(XmlHelpers.DataServices + "PackageSize").Single().Value);
+            var packageHash = entry.Descendants(XmlHelpers.DataServices + "PackageHash").Single().Value;
+            var isLatestVersion = Boolean.Parse(entry.Descendants(XmlHelpers.DataServices + "IsAbsoluteLatestVersion").Single().Value);
+            var published = DateTime.Parse(entry.Descendants(XmlHelpers.DataServices + "Published").Single().Value);
+            var version = entry.Descendants(XmlHelpers.DataServices + "Version").Single().Value;
+
+            return new HostedPackage(package.Name, version, new Uri(content), packageSize, packageHash, isLatestVersion, published);
         }
-        
+
         private HostedPackage(string name, string version, Uri content, long packageSize, string packageHash, bool isLatestVersion, DateTime published)
             : base(name, version)
         {
