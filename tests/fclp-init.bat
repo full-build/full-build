0de248f5732f274a626eeba7617cd7203087eecb
Pierre Chalamet <pierre@chalamet.net>
2014-11-29 14:47:22 +0100

    integration tests enhancements

--- tests/fclp-init.bat
+++ tests/fclp-init.bat
@@ -2,21 +2,21 @@
 setlocal
 
 taskkill /im tgitcache.exe 1>NUL 2>NUL
-if exist rc_init rmdir /s /q rc_init || goto :ko
-mkdir rc_init || goto :ko
+if exist fclp-init rmdir /s /q fclp-init || goto :ko
+mkdir fclp-init || goto :ko
 
 rem setup environment
 fullbuild set config PackageGlobalCache c:\PackageGlobalCache || goto :ko
 fullbuild set config RepoType Git || goto :ko
 fullbuild set config RepoUrl https://github.com/pchalamet/cassandra-sharp-full-build || goto :ko
-fullbuild init workspace rc_init || goto :ko
+fullbuild init workspace fclp-init || goto :ko
 
 rem create workspace
-pushd rc_init
+pushd fclp-init
 
 fullbuild set binrepo c:\BinRepo || goto :ko
 fullbuild add nuget https://www.nuget.org/api/v2/ || goto :ko
-fullbuild add git repo fcpl from https://github.com/VesaKarvonen/Recalled.git || goto :ko
+fullbuild add git repo fcpl from https://github.com/fclp/fluent-command-line-parser.git || goto :ko
 fullbuild clone repo *  || goto :ko
 
 :ok
