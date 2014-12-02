8eed3f64bd23e5fd80f5284b5c0d8502ddb7c50d
Pierre Chalamet <pierre@chalamet.net>
2014-12-03 00:20:40 +0100

    build number for release artifact

--- push-release.bat
+++ push-release.bat
@@ -7,6 +7,7 @@ echo PLATFORM %PLATFORM%
 echo APPVEYOR_BUILD_VERSION %APPVEYOR_BUILD_VERSION%
 echo APPVEYOR_REPO_COMMIT %APPVEYOR_REPO_COMMIT%
 echo APPVEYOR_REPO_COMMIT_MESSAGE  %APPVEYOR_REPO_COMMIT_MESSAGE% 
+echo APPVEYOR_BUILD_NUMBER  %APPVEYOR_BUILD_NUMBER% 
 
 7z a %HERE%%CONFIGURATION%.zip %HERE%\src\bin\%CONFIGURATION%\*
 
@@ -22,7 +23,7 @@ echo APPVEYOR_REPO_COMMIT_MESSAGE  %APPVEYOR_REPO_COMMIT_MESSAGE%
                      --user pchalamet ^
 					 --repo full-build ^
                      --tag %APPVEYOR_BUILD_VERSION% ^
-                     --name "%APPVEYOR_PROJECT_NAME%-net45-anycpu.zip" ^
+                     --name "%APPVEYOR_PROJECT_NAME%-net45-anycpu-%APPVEYOR_BUILD_NUMBER%.zip" ^
                      --file %HERE%%CONFIGURATION%.zip || goto :ko
 
 :ok
