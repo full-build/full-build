bc4efbee9adda0bed99d06bc7747e36612b8fbba
Pierre Chalamet <pierre@chalamet.net>
2014-12-07 22:06:14 +0100

    do not use guid associated with projectreference

--- src/FullBuild/Commands/AnthologyOptimizer.cs
+++ src/FullBuild/Commands/AnthologyOptimizer.cs
@@ -178,9 +178,9 @@ namespace FullBuild.Commands
             {
                 // gather all assemblies from packages in this project
                 var allImportedAssemblies = (from pkgRef in project.PackageReferences
-                                          let pkgdir = pkgsDir.GetDirectory(pkgRef)
-                                          where pkgdir.Exists
-                                          select NuPkg.Assemblies(pkgdir));
+                                             let pkgdir = pkgsDir.GetDirectory(pkgRef)
+                                             where pkgdir.Exists
+                                             select NuPkg.Assemblies(pkgdir));
                 var importedAssemblies = allImportedAssemblies.SelectMany(x => x).Distinct(StringComparer.InvariantCultureIgnoreCase);
 
                 // remove imported assemblies
@@ -191,23 +191,21 @@ namespace FullBuild.Commands
             return anthology;
         }
 
-
-
         private static Anthology UsePackageInsteadOfBinaries(Anthology anthology)
         {
             var pkgsDir = WellKnownFolders.GetPackageDirectory();
             var pkg2assemblies = ((from package in anthology.Packages
-                                  let pkgdir = pkgsDir.GetDirectory(package.Name)
-                                  where pkgdir.Exists
-                                  select new {Name = package.Name, Assemblies = NuPkg.Assemblies(pkgdir)}).ToDictionary(x => x.Name, x => x.Assemblies.ToList()));
+                                   let pkgdir = pkgsDir.GetDirectory(package.Name)
+                                   where pkgdir.Exists
+                                   select new {Name = package.Name, Assemblies = NuPkg.Assemblies(pkgdir)}).ToDictionary(x => x.Name, x => x.Assemblies.ToList()));
 
             foreach (var project in anthology.Projects)
             {
                 foreach (var binary in project.BinaryReferences)
                 {
                     var packages = (from pkg2assembly in pkg2assemblies
-                                   where pkg2assembly.Value.Contains(binary, StringComparer.InvariantCultureIgnoreCase)
-                                   select pkg2assembly).OrderBy(x => x.Value.Count());
+                                    where pkg2assembly.Value.Contains(binary, StringComparer.InvariantCultureIgnoreCase)
+                                    select pkg2assembly).OrderBy(x => x.Value.Count());
                     if (packages.Any())
                     {
                         var newProject = project.RemoveBinaryReference(binary);
@@ -219,6 +217,5 @@ namespace FullBuild.Commands
 
             return anthology;
         }
-
     }
 }
