07d9dc590a93df7e3ddb9d7b05c81572e189a445
Pierre Chalamet <pierre.chalamet@availpro.com>
2014-12-20 00:22:04 +0100

    + is not supported char on windows for filename

--- src/FullBuild/Commands/Packages+Install.cs
+++ src/FullBuild/Commands/Packages+Install.cs
@@ -94,7 +94,7 @@ namespace FullBuild.Commands
                 var portableDirs = libDir.GetDirectories(portablePrefix + "*");
                 foreach (var portableDir in portableDirs)
                 {
-                    var portableDirName = portableDir.Name;
+                    var portableDirName = portableDir.Name.Replace("%2B", "-");
                     var supportedFrameworks = portableDirName.Substring(portablePrefix.Length).Split('+');
                     if (supportedFrameworks.Any(x => x.InvariantEquals(folderToTry)))
                     {
