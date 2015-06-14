e8945510f7d5a83df9dbebf4862029794bfae902
Pierre Chalamet <pierre@chalamet.net>
2015-06-14 14:51:15 +0200

    update nugets

---
+++ src/FsFullBuild.Tests/ConfigurationTests.fs
@@ -0,0 +1,16 @@
+﻿module ConfigurationTests
+
+open NUnit.Framework
+open FsUnit
+open Configuration
+open System.IO
+
+[<Test>]
+let CheckGlobalIniFilename () =
+    let file = new FileInfo("GlobalConfig.ini")
+    let config = GlobalConfigurationFromFile file
+
+    config.BinRepo |> should equal "c:\BinRepo"
+    config.RepoType |> should equal "git"
+    config.RepoUrl |> should equal "https://github.com/pchalamet/full-build"
+    config.PackageGlobalCache |> should equal "c:\PackageGlobalCache"
