32ccbea934cd6412f0d17d04783eaf60cc0d260c
Pierre Chalamet <pierre@chalamet.net>
2015-06-14 09:22:52 +0200

    nuget upgrade

--- src/FsFullBuild/FileExtensions.fs
+++ src/FsFullBuild/FileExtensions.fs
@@ -6,3 +6,7 @@ open System.IO
 let GetSubDirectory (subDir : string) (dir : DirectoryInfo) : DirectoryInfo =
     let newPath = Path.Combine(dir.FullName,subDir)
     new DirectoryInfo(newPath)
+
+let GetFile (dir : DirectoryInfo) (fileName : string) : FileInfo =
+    let fullFileName = Path.Combine(dir.FullName, fileName)
+    new FileInfo(fullFileName)
