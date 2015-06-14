1f0436a50def8003227056147b5c036bd571b54b
Pierre Chalamet <pierre@chalamet.net>
2015-06-14 17:11:40 +0200

    implement workspace config

--- src/FsFullBuild/FileExtensions.fs
+++ src/FsFullBuild/FileExtensions.fs
@@ -7,6 +7,6 @@ let GetSubDirectory (subDir : string) (dir : DirectoryInfo) : DirectoryInfo =
     let newPath = Path.Combine(dir.FullName,subDir)
     new DirectoryInfo(newPath)
 
-let GetFile (dir : DirectoryInfo) (fileName : string) : FileInfo =
+let GetFile (fileName : string) (dir : DirectoryInfo) : FileInfo =
     let fullFileName = Path.Combine(dir.FullName, fileName)
     new FileInfo(fullFileName)
---
+++ src/FsFullBuild/WellknownFolders.fs
@@ -0,0 +1,17 @@
+﻿module WellknownFolders
+
+open System
+open System.IO
+open FileExtensions
+
+
+let rec WorkspaceFolderSearch (dir : DirectoryInfo) =
+    if dir = null || not dir.Exists then failwith "Can't find workspace root directory. Check you are in a workspace."
+    let fbdir = dir |> GetSubDirectory ".full-build"
+    if fbdir.Exists then dir
+    else WorkspaceFolderSearch dir.Parent
+
+let WorkspaceFolder () : DirectoryInfo =
+    let currDir = new DirectoryInfo(Environment.CurrentDirectory)
+    WorkspaceFolderSearch currDir
+
