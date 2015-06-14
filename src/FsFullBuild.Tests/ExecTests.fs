6bd80924c0b2f059450df640470e9d316d6ca943
Pierre Chalamet <pierre@chalamet.net>
2015-06-14 22:27:17 +0200

    test exec

---
+++ src/FsFullBuild.Tests/ExecTests.fs
@@ -0,0 +1,15 @@
+﻿module ExecTests
+
+open NUnit.Framework
+open FsUnit
+open Exec
+open System
+
+[<Test>]
+let CheckExecOk () =
+    Exec "cmd" "/c dir" Environment.CurrentDirectory
+
+[<Test>]
+let CheckExecFailure () =
+    (fun () -> Exec "gloubiboulga" "" System.Environment.CurrentDirectory |> ignore) |> should throw typeof<System.ComponentModel.Win32Exception>
+
