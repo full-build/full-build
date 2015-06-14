35d32473a6f0792ed35eed1f6c57081407a76ae8
Pierre Chalamet <pierre.chalamet@availpro.com>
2015-06-15 00:03:11 +0200

    implement repo clone

--- src/FsFullBuild/Vcs.fs
+++ src/FsFullBuild/Vcs.fs
@@ -4,6 +4,8 @@ open System
 open System.IO
 open Types
 open Exec
+open WellknownFolders
+open FileExtensions
 
 
 let GitCloneRepo (url : Url) (target : DirectoryInfo) =
@@ -14,7 +16,9 @@ let HgCloneRepo (url : Url) (target : DirectoryInfo) =
     let args = sprintf "clone %A %A" url target.FullName
     Exec "hg" args Environment.CurrentDirectory
 
-let VcsCloneRepo (repo : Repository) (target : DirectoryInfo) =
-    match repo with
-    | (Git, _, url) -> GitCloneRepo url target
-    | (Hg, _, url) -> HgCloneRepo url target
+let VcsCloneRepo (wsDir : DirectoryInfo) (repo : Repository) =
+    let (vcs, name, url) = repo
+    let checkoutDir = wsDir |> GetSubDirectory name 
+    match vcs with
+    | Git -> GitCloneRepo url checkoutDir
+    | Hg -> HgCloneRepo url checkoutDir
