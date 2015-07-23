60ccc953b7116b034f121f02bfa1a6dadc152bd5
Pierre Chalamet <pierre@chalamet.net>
2015-07-23 16:42:24 +0200

    begin implementation of projects crawling

--- src/FsFullBuild/Configuration.fs
+++ src/FsFullBuild/Configuration.fs
@@ -52,7 +52,7 @@ let IniDocFromFile (configFile : FileInfo) =
     
 let DefaultGlobalIniFilename () = 
     let userProfileDir = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
-    let configFile = userProfileDir |> GetFile WORKSPACE_CONFIG_FILE
+    let configFile = WORKSPACE_CONFIG_FILE |> GetFile userProfileDir 
     configFile
 
 let GlobalConfigurationFromFile file =
@@ -72,25 +72,3 @@ let GlobalConfigurationFromFile file =
 let GlobalConfig : GlobalConfiguration = 
     let filename = DefaultGlobalIniFilename ()
     GlobalConfigurationFromFile filename
-
-//let DefaultWorkspaceIniFilename () =
-//    let fbDir = WorkspaceConfigFolder ()
-//    let wsConfigFile = fbDir |> GetFile "config"
-//    wsConfigFile
-
-//let RepositoryConfigurationFromSection (section : Mini.IniSection) =
-//    let name = section.Name
-//    let vcs = section.["vcs"].Value
-//    let url = section.["url"].Value
-//    let (ToRepository repo) = (vcs, url, name)
-//    repo
-
-//let WorkspaceConfigurationFromFile file =
-//    let ini = IniDocFromFile file
-//    let repositories = ini |> Seq.map (RepositoryConfigurationFromSection) |> Seq.toList
-//    { Repositories = repositories }
-
-//let WorkspaceConfig () : WorkspaceConfiguration =
-//    let filename = DefaultWorkspaceIniFilename ()
-//    WorkspaceConfigurationFromFile filename
-
