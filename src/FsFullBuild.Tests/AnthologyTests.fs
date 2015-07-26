02dd5e2b96d0e08edf348dbe5da5b77d01a409b7
Pierre Chalamet <pierre.chalamet@availpro.com>
2015-07-26 20:56:39 +0200

    cleanup + binary parsing

--- src/FsFullBuild.Tests/AnthologyTests.fs
+++ src/FsFullBuild.Tests/AnthologyTests.fs
@@ -6,25 +6,23 @@ open NUnit.Framework
 open FsUnit
 open Anthology
 
-
-
 [<Test>]
 let CheckRoundtripAnthology () =
     let expected = {
         Applications = [ ]
-        Binaries = [ { AssemblyName = "System"; HintPath = None }; { AssemblyName = "System.Configuration"; HintPath = None } ]
         Bookmarks = [ { Name = "cassandra-sharp"; Version = "b62e33a6ba39f987c91fdde11472f42b2a4acd94" }; { Name = "cassandra-sharp-contrib"; Version = "e0089100b3c5ca520e831c5443ad9dc8ab176052" } ]
+        Repositories = [ { Vcs = VcsType.Git; Name = "cassandra-sharp"; Url = "https://github.com/pchalamet/cassandra-sharp" }
+                         { Vcs = VcsType.Git; Name = "cassandra-sharp-contrib"; Url = "https://github.com/pchalamet/cassandra-sharp-contrib" } ]
+        Binaries = [ { AssemblyName = "System"; HintPath = None }; { AssemblyName = "System.Configuration"; HintPath = None } ]
         Projects = [ { AssemblyName = "cqlplus"
                        OutputType = OutputType.Exe
                        ProjectGuid = Guid.Parse ("0a06398e-69be-487b-a011-4c0be6619b59")
                        RelativeProjectFile = "cqlplus/cqlplus-net45.csproj"
                        FxTarget = "v4.5"
                        ProjectReferences = [ Guid.Parse ("6f6eb447-9569-406a-a23b-c09b6dbdbe10"); Guid.Parse ("c1d252b7-d766-4c28-9c46-0696f896846c") ]
-                       BinaryReferences = [ "System" ; "System.Data"; "System.Xml"]
+                       BinaryReferences = [ BinaryRef.From "System" ; BinaryRef.From "System.Data"; BinaryRef.From "System.Xml"]
                        PackageReferences = [ ]
                        Repository = "cassandra-sharp" } ]
-        Repositories = [ { Vcs = VcsType.Git; Name = "cassandra-sharp"; Url = "https://github.com/pchalamet/cassandra-sharp" }
-                         { Vcs = VcsType.Git; Name = "cassandra-sharp-contrib"; Url = "https://github.com/pchalamet/cassandra-sharp-contrib" } ]
         }
 
     let file = new FileInfo (Path.GetRandomFileName())
--- src/FsFullBuild.Tests/ProjectParsingTests.fs
+++ src/FsFullBuild.Tests/ProjectParsingTests.fs
@@ -1,33 +1,36 @@
-﻿module ProjectParsingTests
+﻿module ProjectParserTests
 
 open System
 open System.IO
-open ProjectParsing
+open ProjectParser
 open NUnit.Framework
 open FsUnit
 
 [<Test>]
 let CheckBasicParsingCSharp () =
     let file = new FileInfo ("./CSharpProjectSample1.xml")
-    let project = ProjectParsing.ParseProject file.Directory file
-    project.ProjectGuid |> should equal (ProjectParsing.ParseGuid "3AF55CC8-9998-4039-BC31-54ECBFC91396")
+    let prjDescriptor = ProjectParser.ParseProject file.Directory file
+    prjDescriptor.Project.ProjectGuid |> should equal (ProjectParser.ParseGuid "3AF55CC8-9998-4039-BC31-54ECBFC91396")
 
 [<Test>]
 let CheckBasicParsingFSharp () =
     let file = new FileInfo ("./FSharpProjectSample1.xml")
-    let project = ProjectParsing.ParseProject file.Directory file
-    project.ProjectGuid |> should equal (ProjectParsing.ParseGuid "5fde3939-c144-4287-bc57-a96ec2d1a9da")
+    let sources = ProjectParser.ParseProject file.Directory file
+    let prjDescriptor = ProjectParser.ParseProject file.Directory file
+    prjDescriptor.Project.ProjectGuid |> should equal (ProjectParser.ParseGuid "5fde3939-c144-4287-bc57-a96ec2d1a9da")
 
 [<Test>]
 let CheckParseVirginProject () =
     let file = new FileInfo ("./VirginProject.xml")
-    let project = ProjectParsing.ParseProject file.Directory file
-    project.ProjectGuid |> should equal (ProjectParsing.ParseGuid "C1D252B7-D766-4C28-9C46-0696F896846C")
-    project.ProjectReferences |> should equal [ProjectParsing.ParseGuid "c1d252b7-d766-4c28-9c46-0696f896846d"]
+    let project = ProjectParser.ParseProject file.Directory file
+    let sources = ProjectParser.ParseProject file.Directory file
+    let prjDescriptor = ProjectParser.ParseProject file.Directory file
+    prjDescriptor.Project.ProjectReferences |> should equal [ProjectParser.ParseGuid "c1d252b7-d766-4c28-9c46-0696f896846d"]
 
 [<Test>]
 let CheckParseConvertedProject () =
     let file = new FileInfo ("./ConvertedProject.xml")
-    let project = ProjectParsing.ParseProject file.Directory file
-    project.ProjectGuid |> should equal (ProjectParsing.ParseGuid "c1d252b7-d766-4c28-9c46-0696f896846d")
-    project.ProjectReferences |> should equal [ProjectParsing.ParseGuid "6f6eb447-9569-406a-a23b-c09b6dbdbe10"]
+    let project = ProjectParser.ParseProject file.Directory file
+    let sources = ProjectParser.ParseProject file.Directory file
+    let prjDescriptor = ProjectParser.ParseProject file.Directory file
+    prjDescriptor.Project.ProjectReferences |> should equal [ProjectParser.ParseGuid "6f6eb447-9569-406a-a23b-c09b6dbdbe10"]
