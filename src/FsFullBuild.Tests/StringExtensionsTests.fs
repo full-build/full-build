f076e747752190f5d5da5b57a095e1a36d70eac6
Pierre Chalamet <pierre@chalamet.net>
2015-07-28 23:55:44 +0200

    cleanup pattern matching

---
+++ src/FsFullBuild.Tests/StringExtensionsTests.fs
@@ -0,0 +1,33 @@
+﻿module StringExtensionsTests
+
+open StringExtensions
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let CheckEqual () =
+    Match "hello" "hello" |> should equal true
+
+[<Test>]
+let CheckEqualCaseInsensitive () =
+    Match "hello" "HelLo" |> should equal true
+
+[<Test>]
+let CheckNotEqual () =
+    Match "hello" "world" |> should equal false
+
+[<Test>]
+let CheckZeroOrMoreFront () =
+    Match "cassandra-sharp" "*sharp" |> should equal true
+
+[<Test>]
+let CheckZeroOrMoreBack () =
+    Match "cassandra-sharp" "cassandra*" |> should equal true
+
+[<Test>]
+let CheckAll () =
+    Match "hello world world" "HellO * Wor*d" |> should equal true
+
+[<Test>]
+let CheckFailed () =
+    Match "hello world world" "_el?Lo* * Wor?p" |> should equal false
