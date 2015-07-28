022883e1041ae9c416edd75c31e69ce9210d6194
Pierre Chalamet <pierre.chalamet@availpro.com>
2015-07-28 23:55:44 +0200

    cleanup pattern matching

---
+++ src/FsFullBuild.Tests/RepoTests.fs
@@ -0,0 +1,13 @@
+﻿module RepoTests
+
+open NUnit.Framework
+open FsUnit
+open Anthology
+open Repo
+
+[<Test>]
+let CheckFilter () =
+    let repos = [ { Vcs = VcsType.Git; Name = "cassandra-sharp"; Url = "https://github.com/pchalamet/cassandra-sharp" }
+                  { Vcs = VcsType.Git; Name = "cassandra-sharp-contrib"; Url = "https://github.com/pchalamet/cassandra-sharp-contrib" } ]
+  
+    MatchRepo repos "cassandra*" |> should equal repos
