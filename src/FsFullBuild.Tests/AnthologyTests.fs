df20253f8a1ec094399d8ba2f9bb006a01602cf8
Pierre Chalamet <pierre@chalamet.net>
2015-07-30 11:29:50 +0200

    more unit tests

--- src/FsFullBuild.Tests/AnthologyTests.fs
+++ src/FsFullBuild.Tests/AnthologyTests.fs
@@ -5,6 +5,7 @@ open System.IO
 open NUnit.Framework
 open FsUnit
 open Anthology
+open StringHelpers
 
 [<Test>]
 let CheckRoundtripAnthology () =
@@ -23,10 +24,10 @@ let CheckRoundtripAnthology () =
                      { Id="xunit"; Version="1.9.1"; TargetFramework="net45" } ]
         Projects = [ { AssemblyName = "cqlplus"
                        OutputType = OutputType.Exe
-                       ProjectGuid = Guid.Parse ("0a06398e-69be-487b-a011-4c0be6619b59")
+                       ProjectGuid = ParseGuid "0a06398e-69be-487b-a011-4c0be6619b59"
                        RelativeProjectFile = "cqlplus/cqlplus-net45.csproj"
                        FxTarget = "v4.5"
-                       ProjectReferences = [ Guid.Parse ("6f6eb447-9569-406a-a23b-c09b6dbdbe10"); Guid.Parse ("c1d252b7-d766-4c28-9c46-0696f896846c") ]
+                       ProjectReferences = [ ParseGuid "6f6eb447-9569-406a-a23b-c09b6dbdbe10"; ParseGuid "c1d252b7-d766-4c28-9c46-0696f896846c" ]
                        AssemblyReferences = [ "System" ; "System.Data"; "System.Xml"]
                        PackageReferences = [ ]
                        Repository = "cassandra-sharp" } ] }
@@ -39,7 +40,57 @@ let CheckRoundtripAnthology () =
     let antho = LoadAnthologyFromFile file
     antho |> should equal expected
 
+[<Test>]
+let CheckReferences () =
+    // AssemblyRef
+    "TotO42" |> AssemblyRef.From 
+             |> should equal {AssemblyRef.Target = "toto42"}
+    GacAssembly { AssemblyName="TotO42"} |> AssemblyRef.From  
+                                         |> should equal {AssemblyRef.Target = "toto42"}
+    LocalAssembly { AssemblyName="TotO42"; HintPath="c:/tralala" } |> AssemblyRef.From  
+                                                                   |> should equal {AssemblyRef.Target = "toto42"}
 
+    // PackageRef
+    "TotO42" |> PackageRef.From 
+             |> should equal {PackageRef.Target = "toto42"}
+    { Id="TotO42"; Version="Version"; TargetFramework="TargetFramework" } |> PackageRef.From 
+                                                                          |> should equal {PackageRef.Target = "toto42"}
+    
+    "TotO42" |> PackageRef.From 
+             |> should equal {PackageRef.Target = "toto42"}
+    { Id="TotO42"; Version="Version"; TargetFramework="TargetFramework" } |> PackageRef.From 
+                                                                          |> should equal {PackageRef.Target = "toto42"}
 
+    // AssemblyRef
+    { AssemblyName = "cqlplus"
+      OutputType = OutputType.Exe
+      ProjectGuid = ParseGuid "0a06398e-69be-487b-a011-4c0be6619b59"
+      RelativeProjectFile = "cqlplus/cqlplus-net45.csproj"
+      FxTarget = "v4.5"
+      ProjectReferences = [ ParseGuid "6f6eb447-9569-406a-a23b-c09b6dbdbe10"; ParseGuid "c1d252b7-d766-4c28-9c46-0696f896846c" ]
+      AssemblyReferences = [ "System" ; "System.Data"; "System.Xml"]
+      PackageReferences = [ ]
+      Repository = "cassandra-sharp" } |> ProjectRef.From 
+                                       |> should equal {ProjectRef.Target = ParseGuid "0a06398e-69be-487b-a011-4c0be6619b59" }
 
+    // RepositoryRef
+    { Vcs = VcsType.Git
+      Name = "Cassandra-Sharp"
+      Url = "https://github.com/pchalamet/cassandra-sharp" } |> RepositoryRef.From 
+                                                             |> should equal { RepositoryRef.Target = "cassandra-sharp" }
 
+[<Test>]
+let CheckToRepository () =
+    let (ToRepository repoGit) = ("git", "https://github.com/pchalamet/cassandra-sharp", "cassandra-sharp")
+    repoGit |> should equal { Vcs = VcsType.Git
+                              Name = "cassandra-sharp"
+                              Url = "https://github.com/pchalamet/cassandra-sharp" } 
+
+    let (ToRepository repoHg) = ("hg", "https://github.com/pchalamet/cassandra-sharp", "cassandra-sharp")
+    repoHg |> should equal { Vcs = VcsType.Hg
+                             Name = "cassandra-sharp"
+                             Url = "https://github.com/pchalamet/cassandra-sharp" } 
+
+    (fun () -> let (ToRepository repo) = ("pouet", "https://github.com/pchalamet/cassandra-sharp", "cassandra-sharp")
+               ())
+        |> should throw typeof<System.Exception>
