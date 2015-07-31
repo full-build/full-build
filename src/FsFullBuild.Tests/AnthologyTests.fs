2395010a5c34111a52484b3e9bb9b82613525415
Pierre Chalamet <pierre@chalamet.net>
2015-07-31 09:07:41 +0200

    *Ref are opaque

--- src/FsFullBuild.Tests/AnthologyTests.fs
+++ src/FsFullBuild.Tests/AnthologyTests.fs
@@ -43,41 +43,48 @@ let CheckRoundtripAnthology () =
 [<Test>]
 let CheckReferences () =
     // AssemblyRef
-    "TotO42" |> AssemblyRef.From 
-             |> should equal {AssemblyRef.Target = "toto42"}
-    GacAssembly { AssemblyName="TotO42"} |> AssemblyRef.From  
-                                         |> should equal {AssemblyRef.Target = "toto42"}
-    LocalAssembly { AssemblyName="TotO42"; HintPath="c:/tralala" } |> AssemblyRef.From  
-                                                                   |> should equal {AssemblyRef.Target = "toto42"}
+    AssemblyRef.From "toto42" 
+        |> should equal 
+        <| AssemblyRef.From "TOTO42" 
+
+    AssemblyRef.From (GacAssembly { AssemblyName="toto42"}) 
+        |> should equal 
+        <| AssemblyRef.From (LocalAssembly { AssemblyName="TOTO42"; HintPath="c:/tralala" })
 
     // PackageRef
-    "TotO42" |> PackageRef.From 
-             |> should equal {PackageRef.Target = "toto42"}
-    { Id="TotO42"; Version="Version"; TargetFramework="TargetFramework" } |> PackageRef.From 
-                                                                          |> should equal {PackageRef.Target = "toto42"}
-    
-    "TotO42" |> PackageRef.From 
-             |> should equal {PackageRef.Target = "toto42"}
-    { Id="TotO42"; Version="Version"; TargetFramework="TargetFramework" } |> PackageRef.From 
-                                                                          |> should equal {PackageRef.Target = "toto42"}
+    PackageRef.From "TotO42"
+             |> should equal 
+             <| PackageRef.From { Id="TotO42"; Version="Version"; TargetFramework="TargetFramework" } 
 
     // AssemblyRef
-    { AssemblyName = "cqlplus"
-      OutputType = OutputType.Exe
-      ProjectGuid = ParseGuid "0a06398e-69be-487b-a011-4c0be6619b59"
-      RelativeProjectFile = "cqlplus/cqlplus-net45.csproj"
-      FxTarget = "v4.5"
-      ProjectReferences = [ ParseGuid "6f6eb447-9569-406a-a23b-c09b6dbdbe10"; ParseGuid "c1d252b7-d766-4c28-9c46-0696f896846c" ]
-      AssemblyReferences = [ "System" ; "System.Data"; "System.Xml"]
-      PackageReferences = [ ]
-      Repository = "cassandra-sharp" } |> ProjectRef.From 
-                                       |> should equal {ProjectRef.Target = ParseGuid "0a06398e-69be-487b-a011-4c0be6619b59" }
+    ProjectRef.From { AssemblyName = "cqlplus"
+                      OutputType = OutputType.Exe
+                      ProjectGuid = ParseGuid "0a06398e-69be-487b-a011-4c0be6619b59"
+                      RelativeProjectFile = "cqlplus/cqlplus-net45.csproj"
+                      FxTarget = "v4.5"
+                      ProjectReferences = [ ParseGuid "6f6eb447-9569-406a-a23b-c09b6dbdbe10"; ParseGuid "c1d252b7-d766-4c28-9c46-0696f896846c" ]
+                      AssemblyReferences = [ "System" ; "System.Data"; "System.Xml"]
+                      PackageReferences = [ ]
+                      Repository = "cassandra-sharp" }
+        |> should equal 
+        <| ProjectRef.From { AssemblyName = "cqlplus2"
+                             OutputType = OutputType.Dll
+                             ProjectGuid = ParseGuid "{0a06398e-69be-487b-a011-4c0be6619b59}"
+                             RelativeProjectFile = "cqlplus2/cqlplus-net45.csproj"
+                             FxTarget = "v4.0"
+                             ProjectReferences = [ ParseGuid "c1d252b7-d766-4c28-9c46-0696f896846c" ]
+                             AssemblyReferences = [ "System" ; "System.Xml"]
+                             PackageReferences = [ "NUnit" ]
+                             Repository = "cassandra-sharp2" }
 
-    // RepositoryRef
-    { Vcs = VcsType.Git
-      Name = "Cassandra-Sharp"
-      Url = "https://github.com/pchalamet/cassandra-sharp" } |> RepositoryRef.From 
-                                                             |> should equal { RepositoryRef.Target = "cassandra-sharp" }
+//    // RepositoryRef
+    RepositoryRef.From { Vcs = VcsType.Git
+                         Name = "Cassandra-Sharp"
+                         Url = "https://github.com/pchalamet/cassandra-sharp" }
+        |> should equal 
+        <| RepositoryRef.From { Vcs = VcsType.Hg
+                                Name = "Cassandra-Sharp"
+                                Url = "https://github.com/pchalamet/cassandra-sharp2" }
 
 [<Test>]
 let CheckToRepository () =
