85a9b79b8b5902b1b2caa6c47212b7428dd70e3f
Pierre Chalamet <pierre.chalamet@availpro.com>
2015-08-13 23:51:43 +0200

    unit tests:

--- src/FsFullBuild.Tests/IoHelpersTests.fs
+++ src/FsFullBuild.Tests/IoHelpersTests.fs
@@ -13,7 +13,19 @@ let CheckGetSubDirectory () =
     subdir.FullName.Contains("toto") |> should equal true
 
 [<Test>]
+let CheckCreateSubDirectory () =
+    let tmpDir = DirectoryInfo (Path.GetTempPath())
+    let dirName = Guid.NewGuid().ToString("D")
+    let dir = CreateSubDirectory tmpDir dirName
+    dir.Exists |> should equal true
+
+[<Test>]
 let CheckGetFile () =
     let currDir = DirectoryInfo (Environment.CurrentDirectory)
     let file = currDir |> GetFile "toto" 
     file.FullName.Contains("toto") |> should equal true
+
+[<Test>]
+let CheckAddExt () =
+    let file = AddExt "toto" Targets
+    file |> should equal "toto.targets"
---
+++ src/FsFullBuild.Tests/MsBuildHelpersTests.fs
@@ -0,0 +1,34 @@
+﻿module MsBuildHelpersTests
+
+open FsUnit
+open NUnit.Framework
+open MsBuildHelpers
+open System.Xml.Linq
+open Anthology
+open StringHelpers
+
+[<Test>]
+let CheckCast () =
+    let xel = XElement (NsNone + "Toto", 42)
+    let i = !> xel : int
+    i |> should equal 42
+
+[<Test>]
+let CheckProjectPropertyName () =
+    let project = { AssemblyName = "cqlplus"
+                    OutputType = OutputType.Exe
+                    ProjectGuid = ParseGuid "0a06398e-69be-487b-a011-4c0be6619b59"
+                    RelativeProjectFile = "cqlplus/cqlplus-net45.csproj"
+                    FxTarget = "v4.5"
+                    ProjectReferences = [ ParseGuid "6f6eb447-9569-406a-a23b-c09b6dbdbe10"; ParseGuid "c1d252b7-d766-4c28-9c46-0696f896846c" ]
+                    PackageReferences = [ ]
+                    Repository = "cassandra-sharp" }
+
+    let propName = ProjectPropertyName project
+    propName |> should equal "Prj_0a06398e_69be_487b_a011_4c0be6619b59"
+
+[<Test>]
+let CheckPackagePropertyName () =
+    let package = "Rx-Core"
+    let propName = PackagePropertyName package
+    propName |> should equal "FullBuild_Rx_Core_Pkg"
