9cdd1dda5d43ca58091cfd796f26af9c01a617bd
Pierre Chalamet <pierre@chalamet.net>
2015-08-26 07:19:00 +0200

    cleanup simplify assemblies

--- src/FsFullBuild/Simplify.fs
+++ src/FsFullBuild/Simplify.fs
@@ -15,8 +15,15 @@ let AssociatePackage2Projects (file2package : Map<AssemblyRef, PackageRef>) (pro
     res |> Map
 
 
+let (|MatchProject|_|) (projects : Project set) (assName : AssemblyRef) = 
+    let replacementProject = projects |> Seq.tryFind (fun x -> x.Output = assName)
+    match replacementProject with
+    | Some x -> Some (ProjectRef.Bind x)
+    | _ -> None
 
-
+let (|MatchPackage|_|) (file2package : Map<AssemblyRef, PackageRef>) (assName : AssemblyRef) =
+    let replacementPackage = file2package.TryFind assName
+    replacementPackage
 
 let SimplifyAssemblies (projects : Project set) (package2Files : Map<PackageRef, AssemblyRef set>) : Project set =
     let file2package = package2Files |> Map.filter (fun _ nugetFiles -> nugetFiles |> Set.count = 1)
@@ -24,36 +31,25 @@ let SimplifyAssemblies (projects : Project set) (package2Files : Map<PackageRef,
                                         |> Seq.map (fun (id,nugetFiles) -> (nugetFiles |> Seq.head, id))
                                         |> Map
 
-    let rec convertAssemblies  (projects : Project set) (assemblies : AssemblyRef list) (project : Project) =
+    let rec convertAssemblies (projects : Project set) (assemblies : AssemblyRef list) (project : Project) : Project =
         match assemblies with 
-        | assName :: moreAss -> let replacementProject = projects |> Seq.tryFind (fun x -> x.Output = assName)
-                                match replacementProject with 
-                                | Some x -> convertAssemblies projects moreAss { project 
-                                                                                 with AssemblyReferences = Set.remove assName project.AssemblyReferences 
-                                                                                      ProjectReferences = Set.add (ProjectRef.Bind x) project.ProjectReferences }
-                                | _ -> let replacementPackage = file2package.TryFind assName
-                                       match replacementPackage with
-                                       | Some x -> convertAssemblies projects moreAss { project 
-                                                                                        with AssemblyReferences = Set.remove assName project.AssemblyReferences 
-                                                                                             PackageReferences = Set.add x project.PackageReferences }
-                                       | _ -> convertAssemblies projects moreAss project
+        | assName::tail -> let nextConversion = convertAssemblies projects tail
+                           match assName with
+                           | MatchProject projects newProjectRef -> nextConversion { project 
+                                                                                     with AssemblyReferences = Set.remove assName project.AssemblyReferences
+                                                                                          ProjectReferences = Set.add newProjectRef project.ProjectReferences }
+                           | MatchPackage file2package newPackageRef -> nextConversion { project 
+                                                                                         with AssemblyReferences = Set.remove assName project.AssemblyReferences
+                                                                                              PackageReferences = Set.add newPackageRef project.PackageReferences }
+                           | _ -> nextConversion project
         | [] -> project
 
-    let newProjects = projects |> Seq.map (fun x -> convertAssemblies projects (x.AssemblyReferences |> Set.toList) x) |> Set
+    let newProjects = projects |> Set.map (fun x -> convertAssemblies projects (x.AssemblyReferences |> Set.toList) x)
     newProjects
 
 
-let SimplifyPackages (projects : Project set) (package2Files : Map<PackageRef, AssemblyRef set>) (package2packages : Map<PackageRef, PackageRef set>) =
-    seq {
-        let file2package = package2Files |> Map.filter (fun _ nugetFiles -> nugetFiles |> Set.count = 1)
-                                         |> Map.toSeq
-                                         |> Seq.map (fun (id,nugetFiles) -> (nugetFiles |> Seq.head, id))
-                                         |> Map.ofSeq
+//let SimplifyPackages (projects : Project set) (package2packages : Map<PackageRef, PackageRef set>) (package2files : Map<PackageRef, AssemblyRef set>) =
 
-        let package2project = AssociatePackage2Projects file2package projects
-        let packages = (Set << Seq.map fst << Map.toSeq) package2Files
-        ()
-    }
 
 
 let SimplifyAnthology (antho : Anthology) (package2files : Map<PackageRef, AssemblyRef set>) (package2packages : Map<PackageRef, PackageRef set>) =
