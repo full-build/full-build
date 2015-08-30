0252c2643379e021165df4e863879d6b10cab793
Pierre Chalamet <pierre@chalamet.net>
2015-08-30 22:06:53 +0200

    self build

--- build-release.bat
+++ build-release.bat
@@ -1,14 +1,11 @@
-pushd src
-
 set PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 
-.nuget\nuget restore || goto :ko
-msbuild /p:Configuration=Release full-build.sln || goto :ko
+bootstrap\fullbuild package install
+bootstrap\fullbuild view create all using *
+msbuild /p:Configuration=Release *.sln || goto :ko
 
 :ok
-popd
 exit /b 0
 
 :ko
-popd
 exit /b 5
