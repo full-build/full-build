b5ceb3adb63042237d730919e59586ffd58baa2c
Pierre Chalamet <pierre@chalamet.net>
2015-08-30 23:13:59 +0200

    update bootstrap

--- build-release.bat
+++ build-release.bat
@@ -2,7 +2,7 @@ set PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 
 bootstrap\fullbuild package install
 bootstrap\fullbuild view create all using *
-msbuild /p:Configuration=Release *.sln || goto :ko
+msbuild /p:Configuration=Release all.sln || goto :ko
 
 :ok
 exit /b 0
