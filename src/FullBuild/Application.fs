a5a82f76865d3d0baaa2a4d8b3e642bd0ac15652
Pierre Chalamet <pierre@chalamet.net>
2015-08-31 18:40:15 +0200

    add missing files

---
+++ src/FullBuild/Application.fs
@@ -0,0 +1,20 @@
+﻿module Application
+open Anthology
+open Collections
+
+
+let Deploy (names : ApplicationId set) =
+    ()
+
+
+let ListApplicationsContent (apps : Application seq) =
+    seq {
+        for app in apps do
+            yield sprintf "%s" app.Name.Value
+    }
+
+let List () =
+    let antho = Configuration.LoadAnthology ()
+
+    let content = ListApplicationsContent antho.Applications 
+    content |> Seq.iter (fun x -> printfn "%s" x)
