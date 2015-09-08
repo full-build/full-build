2d351afc4eaa5f44624b73bd44e63335452ea9c6
Pierre Chalamet <pierre@chalamet.net>
2015-09-08 09:22:54 +0200

    cleanup

--- src/FullBuild.Tests/RepoTests.fs
+++ src/FullBuild.Tests/RepoTests.fs
@@ -7,7 +7,7 @@ open Repo
 
 [<Test>]
 let CheckFilter () =
-    let repos = [ { Vcs = VcsType.Git; Name = RepositoryId.Bind "cassandra-sharp"; Url = RepositoryUrl "https://github.com/pchalamet/cassandra-sharp" }
-                  { Vcs = VcsType.Git; Name = RepositoryId.Bind "cassandra-sharp-contrib"; Url = RepositoryUrl "https://github.com/pchalamet/cassandra-sharp-contrib" } ]
+    let repos = Set [ { Vcs = VcsType.Git; Name = RepositoryId.Bind "cassandra-sharp"; Url = RepositoryUrl "https://github.com/pchalamet/cassandra-sharp" }
+                      { Vcs = VcsType.Git; Name = RepositoryId.Bind "cassandra-sharp-contrib"; Url = RepositoryUrl "https://github.com/pchalamet/cassandra-sharp-contrib" } ]
   
     MatchRepo repos (RepositoryId.Bind "cassandra*") |> should equal repos
