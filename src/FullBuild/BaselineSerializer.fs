408efc72092d0ee54ebcbd544c51899ad92aa185
Pierre Chalamet <pierre@chalamet.net>
2015-09-11 20:12:38 +0200

    new baseline serialzer

--- src/FullBuild/BaselineSerializer.fs
+++ src/FullBuild/BaselineSerializer.fs
@@ -6,48 +6,40 @@ open System
 open Collections
 open System.Text
 
-//
-//open FSharp.Configuration
-//
-//type BaselineSerializer = YamlConfig<"Baseline.yaml", ReadOnly = true>
+// file format:
+// name1 version
+// name2 version
+// ...
+
+let SerializeBaseline (baseline : Baseline) =
+    seq {
+        for bookmark in baseline.Bookmarks do
+            let repoId = bookmark.Repository.Value
+
+            match bookmark.Version with
+            | BookmarkVersion version -> yield sprintf "commit %s %s" repoId version
+            | Master -> ()
+    }
+
+let (|MatchRepository|) (line : string) =
+    let (repo, version) = Sscanf.sscanf "commit %s %s" line
+    { Repository = RepositoryId.Bind repo; Version = BookmarkVersion version }
+
+let DeserializeBaseline (content : string list) : Baseline =
+    let rec deserializeRepository (repoContent : string list) =
+        match repoContent with
+        | (MatchRepository repo) :: tail -> deserializeRepository tail |> Set.add repo
+        | [] -> Set.empty
+
+    { Bookmarks = deserializeRepository content }
 
 
-//// file format:
-//// name1 version
-//// name2 version
-//// ...
-//
-//let SerializeBaseline (baseline : Baseline) =
-//    seq {
-//        for bookmark in baseline.Bookmarks do
-//            let repoId = bookmark.Repository.Value
-//
-//            match bookmark.Version with
-//            | BookmarkVersion version -> yield sprintf "commit %s %s" repoId version
-//            | Master -> ()
-//    }
-//
-//let (|MatchRepository|) (line : string) =
-//    let (repo, version) = Sscanf.sscanf "commit %s %s" line
-//    { Repository = RepositoryId.Bind repo; Version = BookmarkVersion version }
-//
-//let DeserializeBaseline (content : string list) : Baseline =
-//    let rec deserializeRepository (repoContent : string list) =
-//        match repoContent with
-//        | (MatchRepository repo) :: tail -> deserializeRepository tail |> Set.add repo
-//        | [] -> Set.empty
-//
-//    { Bookmarks = deserializeRepository content }
-//
-//
-//    
-//let Save (filename : FileInfo) (baseline : Baseline) =
-//    let bs = BaselineSerializer().Baseline.[0].
-//
-//    let content = SerializeBaseline baseline
-//    File.WriteAllLines(filename.FullName, content)
-//
-//let Load (filename : FileInfo) : Baseline =
-//    let content = File.ReadAllLines (filename.FullName) |> Seq.toList
-//    DeserializeBaseline content
+    
+let Save (filename : FileInfo) (baseline : Baseline) =
+    let content = SerializeBaseline baseline
+    File.WriteAllLines(filename.FullName, content)
+
+let Load (filename : FileInfo) : Baseline =
+    let content = File.ReadAllLines (filename.FullName) |> Seq.toList
+    DeserializeBaseline content
 
