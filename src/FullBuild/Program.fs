c5e8a7b355fca645decaf0a26f134a9cea293476
Pierre Chalamet <pierre@chalamet.net>
2015-09-13 22:08:20 +0200

    add clean command (pull is no more destructive)

--- src/FullBuild/Program.fs
+++ src/FullBuild/Program.fs
@@ -38,6 +38,7 @@ let main argv =
     | ConvertWorkspace -> Workspace.Convert ()
     | PushWorkspace -> Workspace.Push ()
     | CheckoutWorkspace version -> Workspace.Checkout version.Version
+    | CleanWorkspace -> Workspace.Clean ()
     | PullWorkspace -> Workspace.Pull ()
     | TransformWorkspace -> Workspace.TransformProjects ()
 
