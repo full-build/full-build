987fea12cba82a2402fbe752937205e904edb90f
Pierre Chalamet <pierre@chalamet.net>
2015-09-15 20:35:28 +0200

    update bootstrap

--- update-bootstrap.bat
+++ update-bootstrap.bat
@@ -1,5 +1,8 @@
-copy bin\*.dll bootstrap\ || goto :ko
-copy bin\*.exe bootstrap\ || goto :ko
+copy bin\FSharp.Configuration.dll bootstrap\ || goto :ko
+copy bin\FSharp.Core.dll bootstrap\ || goto :ko
+copy bin\SharpYaml.dll bootstrap\ || goto :ko
+copy bin\FullBuild.exe bootstrap\ || goto :ko
+copy bin\FullBuild.exe.config bootstrap\ || goto :ko
 
 :ok
 exit /b 0
