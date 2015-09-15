f41a2e895eb2c688a2ebbaa80770e33deb3b7f5a
Pierre Chalamet <pierre@chalamet.net>
2015-09-15 20:06:48 +0200

    add assemblyinfo

---
+++ src/FullBuild/AssemblyInfo.fs
@@ -0,0 +1,16 @@
+﻿namespace FullBuild
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<AssemblyTitle("FullBuild")>]
+[<AssemblyDescription("smart build system for .net")>]
+[<AssemblyConfiguration("")>]
+[<AssemblyCompany("")>]
+[<AssemblyProduct("")>]
+[<AssemblyCopyright("Copyright © 2014-2015 Pierre Chalamet")>]
+[<AssemblyTrademark("")>]
+[<AssemblyCulture("")>]
+[<AssemblyVersion("2.0.0.0")>]
+[<AssemblyFileVersion("2.0.0.0")>]
+()
