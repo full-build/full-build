e21199d4361d3e0c3f0b66aeb676bc96b80ae4c6
Pierre Chalamet <pierre@chalamet.net>
2015-09-15 20:48:26 +0200

    update assembly info

--- src/FullBuild/AssemblyInfo.fs
+++ src/FullBuild/AssemblyInfo.fs
@@ -3,14 +3,14 @@
 open System.Reflection
 open System.Runtime.InteropServices
 
-[<AssemblyTitle("FullBuild")>]
-[<AssemblyDescription("smart build system for .net")>]
-[<AssemblyConfiguration("")>]
-[<AssemblyCompany("")>]
-[<AssemblyProduct("")>]
-[<AssemblyCopyright("Copyright © 2014-2015 Pierre Chalamet")>]
-[<AssemblyTrademark("")>]
-[<AssemblyCulture("")>]
-[<AssemblyVersion("2.0.0.0")>]
-[<AssemblyFileVersion("2.0.0.0")>]
+[<assembly: AssemblyTitle("FullBuild")>]
+[<assembly: AssemblyDescription("smart build system for .net")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Copyright © 2014-2015 Pierre Chalamet")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+[<assembly: AssemblyVersion("2.0.0.0")>]
+[<assembly: AssemblyFileVersion("2.0.0.0")>]
 ()
