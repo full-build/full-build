3ecb8afeb83e8fb0e81ce808c7851ad182f8242e
Pierre Chalamet <pierre@chalamet.net>
2015-09-22 22:40:40 +0200

    add 'add nuget' command

--- tests/cs-init.bat
+++ tests/cs-init.bat
@@ -1,4 +1,4 @@
-@echo off
+rem @echo off
 setlocal
 
 call fb-init.bat
@@ -6,21 +6,15 @@ call fb-init.bat
 taskkill /im tgitcache.exe 1>NUL 2>NUL
 if exist cs-init rmdir /s /q cs-init || goto :ko
 
-goto :ok
-
-rem setup environment
-%FULLBUILD% set config binrepo c:\BinRepo
-%FULLBUILD% set config repoType git
-%FULLBUILD% set config repoUrl https://github.com/pchalamet/cassandra-sharp-full-build
-%FULLBUILD% init workspace cs-init || goto :ko
+%FULLBUILD% setup https://github.com/pchalamet/cassandra-sharp-full-build c:\BinRepo cs-init
 
 rem create workspace
 pushd cs-init
 
 %FULLBUILD% add nuget https://www.nuget.org/api/v2/ || goto :ko
-%FULLBUILD% add git repo cassandra-sharp from https://github.com/pchalamet/cassandra-sharp || goto :ko
-%FULLBUILD% add git repo cassandra-sharp-contrib from https://github.com/pchalamet/cassandra-sharp-contrib || goto :ko
-%FULLBUILD% clone repo *  || goto :ko
+%FULLBUILD% add repo cassandra-sharp https://github.com/pchalamet/cassandra-sharp || goto :ko
+%FULLBUILD% add repo cassandra-sharp-contrib https://github.com/pchalamet/cassandra-sharp-contrib || goto :ko
+%FULLBUILD% clone *  || goto :ko
 
 :ok
 echo *** SUCCESSFUL
