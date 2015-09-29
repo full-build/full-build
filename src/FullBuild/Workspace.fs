f2019e7444f52816d5aaf07761e278697c3d6f49
Pierre Chalamet <pierre@chalamet.net>
2015-09-29 13:42:27 +0200

    refactoring

--- src/FullBuild/Workspace.fs
+++ src/FullBuild/Workspace.fs
@@ -36,29 +36,6 @@ open System.Xml.Linq
 open Collections
 open System
 
-let private FindKnownProjects (repoDir : DirectoryInfo) =
-    [AddExt "*" CsProj
-     AddExt "*" VbProj
-     AddExt "*" FsProj] |> Seq.map (fun x -> repoDir.EnumerateFiles (x, SearchOption.AllDirectories)) 
-                        |> Seq.concat
-
-let private ProjectCanBeProcessed (fileName : FileInfo) =
-    let xdoc = XDocument.Load (fileName.FullName)
-    let fbIgnore = !> xdoc.Descendants(NsMsBuild + "FullBuildIgnore").FirstOrDefault() : string
-    match bool.TryParse(fbIgnore) with
-    | (true, x) -> not <| x
-    | _ -> true
-
-let private ParseRepositoryProjects (parser) (repoRef : RepositoryId) (repoDir : DirectoryInfo) =
-    repoDir |> FindKnownProjects 
-            |> Seq.filter ProjectCanBeProcessed
-            |> Seq.map (parser repoDir repoRef)
-
-let private ParseWorkspaceProjects (parser) (wsDir : DirectoryInfo) (repos : Repository seq) = 
-    repos |> Seq.map (fun x -> GetSubDirectory x.Name.toString wsDir) 
-          |> Seq.filter (fun x -> x.Exists) 
-          |> Seq.map (fun x -> ParseRepositoryProjects parser (RepositoryId.from(x.Name)) x)
-          |> Seq.concat
 
 let Init (path : string) (uri : RepositoryUrl) = 
     let wsDir = DirectoryInfo(path)
@@ -109,10 +86,12 @@ let rec DisplayConflicts (conflicts : Simplify.ConflictType list) =
     | [] -> ()
 
 
+// this function has 2 side effects:
+// * update paket.dependencies (both sources and packages)
 let Index () = 
     let wsDir = Env.GetFolder Env.Workspace
     let antho = LoadAnthology()
-    let projects = ParseWorkspaceProjects ProjectParsing.ParseProject wsDir antho.Repositories
+    let projects = Indexation.ParseWorkspaceProjects ProjectParsing.ParseProject wsDir antho.Repositories
 
     // merge packages
     let foundPackages = projects |> Seq.map (fun x -> x.Packages) 
