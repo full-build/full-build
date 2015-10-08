bd24ab8e69df7623bc82655dae928fa3f72e6ab7
Pierre Chalamet <pierre@chalamet.net>
2015-10-08 07:30:10 +0200

    set correct version number

--- src/FullBuild/AssemblyInfo.fs
+++ src/FullBuild/AssemblyInfo.fs
@@ -3,13 +3,13 @@
 open System.Reflection
 
 [<assembly: AssemblyTitle("FullBuild")>]
-[<assembly: AssemblyDescription("smart build system for .net")>]
+[<assembly: AssemblyDescription("Smart build system for .net")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("")>]
 [<assembly: AssemblyCopyright("Copyright © 2014-2015 Pierre Chalamet")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("2.0.0.0")>]
+[<assembly: AssemblyFileVersion("2.0.0.0")>]
 ()
