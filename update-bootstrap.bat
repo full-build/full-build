31e67ec03776e31fbad0a3a0d8ffa4ad2b01b941
Pierre Chalamet <pierre@chalamet.net>
2015-10-22 23:31:56 +0200

    complete publish implementation

--- update-bootstrap.bat
+++ update-bootstrap.bat
@@ -1,8 +1,8 @@
-copy bin\FSharp.Configuration.dll bootstrap\ || goto :ko
-copy bin\FSharp.Core.dll bootstrap\ || goto :ko
-copy bin\SharpYaml.dll bootstrap\ || goto :ko
-copy bin\FullBuild.exe bootstrap\ || goto :ko
-copy bin\FullBuild.exe.config bootstrap\ || goto :ko
+copy bin\fullbuild\FSharp.Configuration.dll bootstrap\ || goto :ko
+copy bin\fullbuild\FSharp.Core.dll bootstrap\ || goto :ko
+copy bin\fullbuild\SharpYaml.dll bootstrap\ || goto :ko
+copy bin\fullbuild\FullBuild.exe bootstrap\ || goto :ko
+copy bin\fullbuild\FullBuild.exe.config bootstrap\ || goto :ko
 goto :ok
 
 :ok
