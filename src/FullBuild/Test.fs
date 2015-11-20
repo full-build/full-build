76d148c22aa9153075d3d49e490d7a8d2e72a59d
Pierre Chalamet <pierre@chalamet.net>
2015-11-20 12:24:11 +0100

    unset output

--- src/FullBuild/Test.fs
+++ src/FullBuild/Test.fs
@@ -7,9 +7,8 @@ open Anthology
 
 let runnerNUnit (matches : string seq) =
     let wsDir = GetFolder Env.Workspace
-    let outFile = wsDir |> GetFile "TestResult.xml"
     let files = matches |> Seq.fold (fun s t -> sprintf @"%s %A" s t) ""
-    let args = sprintf @"%s --where ""cat != Integration"" --noheader --result=nunit2 --output=%A" files outFile.FullName
+    let args = sprintf @"%s --where ""cat != Integration"" --noheader --result=nunit2" files
     printf "%s" args
     Exec.Exec "nunit3-console" args wsDir
 
