e3db73d6181b4bae8b7a37db41493111eef83e0b
Pierre Chalamet <pierre@chalamet.net>
2015-11-24 22:31:32 +0100

    unit test using full-build

--- build-release.bat
+++ build-release.bat
@@ -5,6 +5,7 @@ set PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 bootstrap\fullbuild install || goto :ko
 bootstrap\fullbuild add-view fullbuild * || goto :ko
 bootstrap\fullbuild rebuild fullbuild || goto :ko
+bootstrap\fullbuild test * || goto :ko
 goto :ok
 
 :ok
