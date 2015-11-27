921a6eabcf70c401ca590f1bfd8fc377573d1c2e
Pierre Chalamet <pierre@chalamet.net>
2015-11-27 22:29:34 +0100

    command line parsing refactoring

--- build-release.bat
+++ build-release.bat
@@ -2,8 +2,8 @@ echo on
 setlocal
 set PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 
-bootstrap\fullbuild install || goto :ko
-bootstrap\fullbuild add-view fullbuild * || goto :ko
+bootstrap\fullbuild install package || goto :ko
+bootstrap\fullbuild add view fullbuild * || goto :ko
 bootstrap\fullbuild rebuild fullbuild || goto :ko
 goto :ok
 
