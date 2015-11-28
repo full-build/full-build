88861be96a7d972eff044a88a71b143eef51770e
Pierre Chalamet <pierre@chalamet.net>
2015-11-29 00:25:14 +0100

    comand line refactoring

--- src/FullBuild/CommandLine.fs
+++ src/FullBuild/CommandLine.fs
@@ -1,4 +1,18 @@
-﻿module CommandLine
+﻿//   Copyright 2014-2015 Pierre Chalamet
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+module CommandLine
 open Anthology
 open Collections
 open StringHelpers
@@ -17,7 +31,8 @@ type CheckoutWorkspace =
     { Version : string }
 
 type CloneRepositories = 
-    { Filters : RepositoryId set }
+    { Filters : RepositoryId set 
+      Shallow : bool}
 
 type TestAssemblies = 
     { Filters : string list 
@@ -56,6 +71,13 @@ type GraphView =
 type Exec = 
     { Command : string }
 
+type AddRepository =
+    {
+        Repo : RepositoryId
+        Url : RepositoryUrl
+        Type : VcsType
+    }
+
 type Command = 
     | Version
     | Usage
@@ -76,7 +98,7 @@ type Command =
 
     // repository
     | ListRepositories
-    | AddRepository of RepositoryId * RepositoryUrl
+    | AddRepository of AddRepository
     | CloneRepositories of CloneRepositories
     | DropRepository of RepositoryId
 
--- src/FullBuild/Vcs.fs
+++ src/FullBuild/Vcs.fs
@@ -1,27 +1,17 @@
-﻿// Copyright (c) 2014-2015, Pierre Chalamet
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of Pierre Chalamet nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL PIERRE CHALAMET BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+﻿//   Copyright 2014-2015 Pierre Chalamet
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 module Vcs
 
 open System
@@ -102,8 +92,11 @@ let private hgIs (uri : RepositoryUrl) =
 
 
 
-let private gitClone (isGerrit : bool) (target : DirectoryInfo) (url : string) = 
-    let args = sprintf @"clone --depth=1 %A %A" url target.FullName
+let private gitClone (isGerrit : bool) (shallow : bool) (target : DirectoryInfo) (url : string) = 
+    let depth = if shallow then "--depth=1"
+                else ""
+
+    let args = sprintf @"clone %s %A %A" depth url target.FullName
     let currDir = DirectoryInfo(Environment.CurrentDirectory)
     checkedExec "git" args currDir
 
@@ -156,9 +149,10 @@ let ApplyVcs (wsDir : DirectoryInfo) (repo : Repository) gitFun hgFun =
     f repoDir
 
 
-let VcsCloneRepo (wsDir : DirectoryInfo) (repo : Repository) =
-    let gitCloneFunc =  gitClone (repo.Vcs = VcsType.Gerrit)
-    (ApplyVcs wsDir repo gitCloneFunc hgClone) repo.Url.toString
+let VcsCloneRepo (wsDir : DirectoryInfo) (shallow : bool) (repo : Repository) =
+    let gitCloneFunc =  gitClone (repo.Vcs = VcsType.Gerrit) shallow
+    let hgCloneFunc = hgClone
+    (ApplyVcs wsDir repo gitCloneFunc hgCloneFunc) repo.Url.toString
 
 let VcsTip (wsDir : DirectoryInfo) (repo : Repository) = 
     ApplyVcs wsDir repo gitTip hgTip
