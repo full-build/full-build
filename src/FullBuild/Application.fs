2efa5f5db0024564d52c5101644d3320c81b20f2
Pierre Chalamet <pierre@chalamet.net>
2016-01-08 21:23:58 +0100

    refactoring to support fake

--- src/FullBuild/Application.fs
+++ src/FullBuild/Application.fs
@@ -15,59 +15,7 @@
 module Application
 open Anthology
 open Collections
-open System.IO
-open Env
-open IoHelpers
-open System.Xml.Linq
-open MsBuildHelpers
 
-
-let private checkErrorCode err =
-    if err <> 0 then failwithf "Process failed with error %d" err
-
-let private checkedExec = 
-    Exec.Exec checkErrorCode
-
-
-let publishAppCopy (app : Anthology.Application) =
-    let wsDir = GetFolder Env.Workspace
-    let antho = Configuration.LoadAnthology ()
-    let project = antho.Projects |> Seq.find (fun x -> x.ProjectId = app.Project)
-    let repoDir = wsDir |> GetSubDirectory (project.Repository.toString)
-    if repoDir.Exists then
-        let projFile = repoDir |> GetFile project.RelativeProjectFile.toString 
-        let args = sprintf "/nologo /t:FBPublish /p:SolutionDir=%A /p:FBApp=%A %A" wsDir.FullName app.Name.toString projFile.FullName
-
-        if Env.IsMono () then checkedExec "xbuild" args wsDir
-        else checkedExec "msbuild" args wsDir
-    else
-        printfn "[WARNING] Can't publish application %A without repository" app.Name.toString
-
-let publishAppZip (app : Anthology.Application) =
-    let tmpApp = { app
-                   with Name = ApplicationId.from "tmpzip"
-                        Publisher = PublisherType.Copy }
-
-    publishAppCopy tmpApp
-
-    let appDir = GetFolder Env.AppOutput
-    let sourceFolder = appDir |> GetSubDirectory (tmpApp.Name.toString)
-    let targetFile = appDir |> GetFile (AddExt Extension.Zip app.Name.toString)
-    if targetFile.Exists then targetFile.Delete()
-
-    System.IO.Compression.ZipFile.CreateFromDirectory(sourceFolder.FullName, targetFile.FullName, Compression.CompressionLevel.Optimal, false)
-    sourceFolder.Delete(true)
-    
-
-let choosePublisher (pubType : PublisherType) appCopy appZip =
-    let publish = match pubType with
-                  | PublisherType.Copy -> appCopy
-                  | PublisherType.Zip -> appZip
-    publish
-
-
-let publishWithPublisher (pubType : PublisherType) =
-    choosePublisher pubType publishAppCopy publishAppZip
 
 
 let Publish (filters : string list) =
@@ -82,7 +30,7 @@ let Publish (filters : string list) =
 
     for appName in matches do
         let app = antho.Applications |> Seq.find (fun x -> x.Name = appName)
-        (publishWithPublisher app.Publisher) app
+        (Publishers.PublishWithPublisher app.Publisher) app
 
 let List () =
     let antho = Configuration.LoadAnthology ()
