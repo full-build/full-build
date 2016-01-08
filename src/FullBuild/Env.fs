36a0a35b95186ca2efcf75aa2528c62a8c38fbdc
Pierre Chalamet <pierre@chalamet.net>
2016-01-09 00:17:09 +0100

    license update

--- src/FullBuild/Env.fs
+++ src/FullBuild/Env.fs
@@ -1,4 +1,4 @@
-﻿//   Copyright 2014-2015 Pierre Chalamet
+﻿//   Copyright 2014-2016 Pierre Chalamet
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -98,5 +98,5 @@ let CheckLicense () =
 
     let licContent = File.ReadAllText (licFile.FullName)
     let guid = StringHelpers.GenerateGuidFromString licContent
-    let licGuid = StringHelpers.ParseGuid "dc2991a6-9f65-a56c-26ac-3ba65d875d80"
+    let licGuid = StringHelpers.ParseGuid "21a734e7-1308-06de-3905-7708ed4c4dbc"
     if guid <> licGuid then failwithf "Please ensure original LICENSE.txt is available."
