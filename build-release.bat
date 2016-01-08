0da561aed4174b1638b3b10f808eec7f2bbff7fb
Pierre Chalamet <pierre@chalamet.net>
2016-01-08 21:44:48 +0100

    use version command instead of appveyor

--- build-release.bat
+++ build-release.bat
@@ -1,7 +1,16 @@
 echo on
 setlocal
-set PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 
+set VERSION=%1
+set VERSION=%1
+if [%VERSION%] == [] set VERSION=0.0.0.* 
+echo building version [%VERSION%]
+
+set PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
+set HERE=%~dp0
+
+
+bootstrap\fullbuild init --version %VERSION% ssh://none %HERE% || goto :ko
 bootstrap\fullbuild install package || goto :ko
 bootstrap\fullbuild add view fullbuild * || goto :ko
 bootstrap\fullbuild rebuild fullbuild || goto :ko
