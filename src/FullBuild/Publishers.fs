a4d0c7001cedfd6ee1feeb805fe38fb34ed15c37
Pierre Chalamet <pierre@chalamet.net>
2016-01-08 21:23:58 +0100

    refactoring to support fake

---
+++ src/FullBuild/Publishers.fs
@@ -0,0 +1,63 @@
+﻿module Publishers
+open Anthology
+open IoHelpers
+open Env
+open System.IO
+
+
+let private checkErrorCode err =
+    if err <> 0 then failwithf "Process failed with error %d" err
+
+let private checkedExec = 
+    Exec.Exec checkErrorCode
+
+
+
+let publishCopy (app : Anthology.Application) =
+    let wsDir = GetFolder Env.Workspace
+    let antho = Configuration.LoadAnthology ()
+    let project = antho.Projects |> Seq.find (fun x -> x.ProjectId = app.Project)
+    let repoDir = wsDir |> GetSubDirectory (project.Repository.toString)
+    if repoDir.Exists then
+        let projFile = repoDir |> GetFile project.RelativeProjectFile.toString 
+        let args = sprintf "/nologo /t:FBPublish /p:SolutionDir=%A /p:FBApp=%A %A" wsDir.FullName app.Name.toString projFile.FullName
+
+        if Env.IsMono () then checkedExec "xbuild" args wsDir
+        else checkedExec "msbuild" args wsDir
+    else
+        printfn "[WARNING] Can't publish application %A without repository" app.Name.toString
+
+let publishZip (app : Anthology.Application) =
+    let tmpApp = { app
+                   with Name = ApplicationId.from "tmpzip"
+                        Publisher = PublisherType.Copy }
+
+    publishCopy tmpApp
+
+    let appDir = GetFolder Env.AppOutput
+    let sourceFolder = appDir |> GetSubDirectory (tmpApp.Name.toString)
+    let targetFile = appDir |> GetFile (AddExt Extension.Zip app.Name.toString)
+    if targetFile.Exists then targetFile.Delete()
+
+    System.IO.Compression.ZipFile.CreateFromDirectory(sourceFolder.FullName, targetFile.FullName, Compression.CompressionLevel.Optimal, false)
+    sourceFolder.Delete(true)
+    
+
+let publishFake (app : Anthology.Application) =
+    ()
+
+
+
+
+
+let choosePublisher (pubType : PublisherType) appCopy appZip appFake =
+    let publish = match pubType with
+                  | PublisherType.Copy -> appCopy
+                  | PublisherType.Zip -> appZip
+                  | PublisherType.Fake -> appFake
+    publish
+
+
+let PublishWithPublisher (pubType : PublisherType) =
+    choosePublisher pubType publishCopy publishZip publishFake
+
---
+++ src/FullBuild/TestRunners.fs
@@ -0,0 +1,41 @@
+﻿module TestRunners
+open Anthology
+open Env
+
+
+
+
+let checkErrorCode err =
+    if err < 0 then failwithf "Process failed with error %d" err
+
+let private checkedExec = 
+    Exec.Exec checkErrorCode
+
+let excludeListToArgs (excludes : string list) =
+    match excludes with
+    | [] -> ""
+    | [x] -> let excludeArgs = sprintf "cat != %s" x
+             sprintf "--where %A" excludeArgs
+    | x :: tail -> let excludeArgs = excludes |> Seq.fold (fun s t -> sprintf "%s && cat != %s" s t) ("")
+                   sprintf "--where %A" excludeArgs
+
+
+
+let runnerNUnit (matches : string seq) (excludes : string list) =
+    let wsDir = GetFolder Env.Workspace
+    let files = matches |> Seq.fold (fun s t -> sprintf @"%s %A" s t) ""
+    let excludeArgs = excludeListToArgs excludes
+    let args = sprintf @"%s %s --noheader ""--result=TestResult.xml;format=nunit2""" files excludeArgs 
+    checkedExec "nunit3-console" args wsDir
+
+let runnerFake (matches : string seq) (excludes : string list) =
+    ()
+
+let chooseTestRunner (runnerType : TestRunnerType) nunitRunner fakeRunner =
+    let runner = match runnerType with
+                 | TestRunnerType.NUnit -> nunitRunner
+                 | TestRunnerType.Fake -> fakeRunner
+    runner
+
+let TestWithTestRunner (runnerType : TestRunnerType) =
+    chooseTestRunner runnerType runnerNUnit runnerFake
