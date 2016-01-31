6ea2fffa7df1a4b8146d0c2ce2ac93752b8b6789
Pierre Chalamet <pierre@chalamet.net>
2016-01-31 22:36:48 +0100

    add missing files

---
+++ src/FullBuild.Tests/ViewSerializerTests.fs
@@ -0,0 +1,17 @@
+﻿module ViewSerializerTests
+
+open FsUnit
+open NUnit.Framework
+open Anthology
+
+[<Test>]
+let CheckSaveLoadBaseline () =
+    let view1 = { Filters = ["cassandra-sharp/*"] |> Set
+                  Builder = BuilderType.MSBuild
+                  Parameters = ["--mt"; "--debug" ] |> Set }
+
+    let res = ViewSerializer.SerializeView view1
+    printfn "%s" res
+
+    let view2 = ViewSerializer.DeserializeView res
+    view2 |> should equal view1
