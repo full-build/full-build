a26ddf0842ba246b6d0d8989858e9a62a60e55dd
Pierre Chalamet <pierre@chalamet.net>
2016-03-02 22:50:47 +0100

    generate binding redict on publish

--- src/FullBuild/MsBuildHelpers.fs
+++ src/FullBuild/MsBuildHelpers.fs
@@ -20,6 +20,8 @@ let NsMsBuild = XNamespace.Get("http://schemas.microsoft.com/developer/msbuild/2
 
 let NsDgml = XNamespace.Get("http://schemas.microsoft.com/vs/2009/dgml")
 
+let NsRuntime = XNamespace.Get("urn:schemas-microsoft-com:asm.v1")
+
 let NsNone = XNamespace.None
 
 let inline (!>) (x : ^a) : ^b = (((^a or ^b) : (static member op_Explicit : ^a -> ^b) x))
