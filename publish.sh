67bc8871347805ce967df16c1875199c76371610
Pierre Chalamet <pierre@chalamet.net>
2016-03-27 00:43:44 +0100

    fix build script #56

--- publish.sh
+++ publish.sh
@@ -8,9 +8,7 @@ function failure
 function publish
 {
   mono dotnet/fullbuild/bin/fullbuild.exe publish "*" || failure
-  
-  rm -f apps/full-build/*
-  cp apps/full-build/* apps/full-build
+  cp apps/full-build/* bootstrap/
 }
 
 
