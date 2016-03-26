e5de5f7a09ce92e5b4ea8c7b9225d2ad85558c32
Pierre Chalamet <pierre@chalamet.net>
2016-03-26 22:48:36 +0100

    fix publish app #57

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ function failure
 
 function publish
 {
-  mono src/fullbuild/bin/fullbuild.exe publish * || failure
+  mono dotnet/fullbuild/bin/fullbuild.exe publish * || failure
   
   rm -f apps/full-build/*
   cp apps/full-build/* apps/full-build
