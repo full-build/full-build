3a764c85a47c0852178c42aaabc41187214b2c24
Pierre Chalamet <pierre@chalamet.net>
2016-03-26 19:52:48 +0100

    fix issue with * - #45

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ function failure
 function build
 {
   mono bootstrap/fullbuild.exe install || failure
-  mono bootstrap/fullbuild.exe view all * || failure
+  mono bootstrap/fullbuild.exe view all "*" || failure
   mono bootstrap/fullbuild.exe rebuild --version $1 all || failure
 }
 
