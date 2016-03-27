e4f3aa250559fc80376f995328ce57bc8563cf6e
Pierre Chalamet <pierre@chalamet.net>
2016-03-27 15:38:14 +0200

    fix bootstrap on windows #60

--- build-bootstrap.cmd
+++ build-bootstrap.cmd
@@ -6,13 +6,14 @@ set HERE=%~dp0
 pushd %HERE%\.full-build
 %HERE%\tools\paket.exe install || goto :failure
 popd
-robocopy bootstrap\bin %HERE%\.full-build\bin /MIR
-robocopy bootstrap\views .full-build\views /MIR
-robocopy bootstrap\projects .full-build\projects /MIR
-robocopy bootstrap\packages .full-build\packages /MIR
+robocopy bootstrap\bin %HERE%\.full-build\bin /E
+robocopy bootstrap\views .full-build\views /E
+robocopy bootstrap\projects .full-build\projects /E
+robocopy bootstrap\packages .full-build\packages /E
 copy bootstrap\bootstrap.sln %HERE%
 
 msbuild /t:Rebuild bootstrap.sln || goto :failure
+call publish.cmd || goto :failure
 
 :ok
 exit /b 0
