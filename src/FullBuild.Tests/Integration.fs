711cc18d6d9dbc448043c4659cc5bbcae211e411
Pierre Chalamet <pierre@chalamet.net>
2016-09-24 18:15:36 +0200

    add integration tests

--- src/FullBuild.Tests/Integration.fs
+++ src/FullBuild.Tests/Integration.fs
@@ -22,11 +22,26 @@ let runFB (args : string) =
     proc.WaitForExit()
     if proc.ExitCode <> 0 then failwithf "Process failed with error %d" proc.ExitCode
 
-
 [<Test>]
-let CheckBuildSources () = 
+let CheckSourceBuildIsSameAsBinaryBuild () = 
+    let expectedFiles = [ "libproject.dll"
+                          "libproject.pdb"
+                          "mainproject.exe"
+                          "mainproject.exe.config"
+                          "mainproject.pdb"
+                          "Mono.Cecil.dll"
+                          "Mono.Cecil.Mdb.dll"
+                          "Mono.Cecil.Pdb.dll"
+                          "Mono.Cecil.Rocks.dll" ] |> set
+
     runFB "view testsrc tests/*"
-    runFB "rebuild --mt testsrc"
+    runFB "rebuild --mt testsrc"    
+
+    let outputDir = TestContext.CurrentContext.TestDirectory + "/../../../tests/MainProject/bin" |> DirectoryInfo
+    let outputFileSrc = outputDir.EnumerateFiles () |> Seq.map (fun x -> x.Name) |> set
+    outputFileSrc |> should equal expectedFiles
 
     runFB "view testbin tests/mainproject"
     runFB "rebuild --mt testbin"
+    let outputFileBin = outputDir.EnumerateFiles () |> Seq.map (fun x -> x.Name) |> set
+    outputFileBin |> should equal expectedFiles
