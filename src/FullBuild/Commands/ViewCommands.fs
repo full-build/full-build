bd61b9b84a212ead59a7907c21531c2233a5def9
Pierre Chalamet <pierre@chalamet.net>
2016-10-11 07:35:41 +0200

    update file banner

--- src/FullBuild/Commands/ViewCommands.fs
+++ src/FullBuild/Commands/ViewCommands.fs
@@ -19,94 +19,6 @@ open IoHelpers
 open Collections
 open Graph
 
-//
-//
-//let checkErrorCode err =
-//    if err <> 0 then failwithf "Process failed with error %d" err
-//
-//
-//
-//let filterClonedRepositories (wsDir : System.IO.DirectoryInfo) (repo : Repository) =
-//    let repoDir = wsDir |> GetSubDirectory repo.Name.toString
-//    let exists = repoDir.Exists
-//    exists
-//
-//
-//let private adaptViewFilter (filter : string) =
-//    if filter.Contains("/") || filter.Contains("*") then filter
-//    else filter + "/*"
-//
-//
-//let ComputeModifiedRepositories (availableRepos : RepositoryId set) (old : Bookmark set) (current : Bookmark set) =
-//    seq {
-//        let oldBookmarks = old |> Seq.map (fun x -> (x.Repository, x.Version)) |> Map
-//        for currBook in current do
-//            let oldBook = oldBookmarks |> Map.tryFind currBook.Repository
-//            match oldBook with
-//            | Some x -> if x <> currBook.Version && availableRepos.Contains(currBook.Repository) then
-//                            yield currBook.Repository.toString // modified
-//            | _ -> yield currBook.Repository.toString // new
-//    } |> Set.ofSeq
-//
-//let GetModifiedFilter (view : View) =
-//// UNDONE
-////    let wsDir = Env.GetFolder Folder.Workspace
-////    let antho = Configuration.LoadAnthology ()
-////
-////    if view.Modified then
-////        let clonedRepos = antho.Repositories |> Set.map (fun x -> x.Repository)
-////                                             |> Set.filter (filterClonedRepositories wsDir)
-////        let availableRepos = clonedRepos |> Set.map (fun x -> x.Name)
-////
-////        let current = Repo.CollectRepoHash antho.Vcs wsDir clonedRepos
-////        let baseline = Configuration.LoadBaseline ()
-////        ComputeModifiedRepositories availableRepos baseline.Bookmarks current
-////    else
-//        Set.empty
-//
-//let FindViewProjects (view : View) =
-//    let wsDir = Env.GetFolder Folder.Workspace
-//    let antho = Configuration.LoadAnthology ()
-//
-//    // select only available repositories
-//    let availableRepos = antho.Repositories |> Set.map (fun x -> x.Repository)
-//                                            |> Set.filter (filterClonedRepositories wsDir)
-//                                            |> Set.map(fun x -> x.Name)
-//
-//    // load back filter & generate view accordingly
-//    let repoFilters = view.Filters
-//    let modifiedFilters = GetModifiedFilter view
-//    let selectionFilters = repoFilters |> Set.union modifiedFilters |> Set.map adaptViewFilter
-//
-//    // build: <repository>/<project>
-//    let projects = antho.Projects
-//                   |> Seq.filter (fun x -> availableRepos |> Set.contains x.Repository)
-//                   |> Seq.map (fun x -> (sprintf "%s/%s" x.Repository.toString x.Output.toString, x.ProjectId))
-//                   |> Map
-//    let projectNames = projects |> Seq.map (fun x -> x.Key) |> set
-//
-//    let matchRepoProject filter =
-//        projectNames |> Set.filter (fun x -> PatternMatching.Match x filter)
-//
-//    let matches = selectionFilters |> Set.map matchRepoProject
-//                                   |> Set.unionMany
-//    let selectedProjectGuids = projects |> Map.filter (fun k _ -> Set.contains k matches)
-//                                        |> Seq.map (fun x -> x.Value)
-//                                        |> Set
-//    let parents = if view.ReferencedBy then AnthologyGraph.CollectAllParents antho.Projects selectedProjectGuids
-//                  else Set.empty
-//    let allProjectSet = selectedProjectGuids |> Set.union parents
-//
-//    // find projects
-//    let antho = Configuration.LoadAnthology ()
-//    let projectRefs = match view.Dependencies with
-//                      | true -> AnthologyGraph.ComputeProjectSelectionClosureSourceOnly antho.Projects allProjectSet |> Set
-//                      | _ -> AnthologyGraph.ComputeProjectSelectionClosure antho.Projects allProjectSet |> Set
-//
-//    let projects = antho.Projects |> Set.filter (fun x -> projectRefs |> Set.contains x.ProjectId)
-//    projects
-//
-//
 //let FindViewApplications viewId =
 //    let antho = Configuration.LoadAnthology ()
 //    let viewProjectIds = viewId |> Configuration.LoadView 
@@ -299,8 +211,6 @@ let Add (cmd : Commands.AddView) =
     SaveFileIfNecessary slnFile slnContent
 
 
-
-
 let Drop name =
     let graph = Configuration.LoadAnthology() |> Graph.from
     let view = graph.Views |> Seq.tryFind (fun x -> x.Name = name)
@@ -362,7 +272,6 @@ let Open (cmd : Commands.OpenView) =
 let Graph (cmd : Commands.GraphView) =
     let graph = Configuration.LoadAnthology() |> Graph.from
     let view = graph.Views |> Seq.find (fun x -> x.Name = cmd.Name)
-    let graph = Configuration.LoadAnthology() |> Graph.from
     let projects = view.Projects |> set
     let wsDir = Env.GetFolder Env.Folder.Workspace
     let graphFile = wsDir |> GetSubDirectory (AddExt Dgml cmd.Name)
