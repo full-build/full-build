52d7f0dc377ce4970aed5345edc068a6ba296588
Pierre Chalamet <pierre@chalamet.net>
2016-10-15 19:35:17 +0200

    because MSBuild is the correct name

--- src/FullBuild/Generators/MsBuild.fs
+++ src/FullBuild/Generators/MsBuild.fs
@@ -12,7 +12,7 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-module Generators.MsBuild
+module Generators.MSBuild
 open System.IO
 open System.Xml.Linq
 open System
