879424345126178beb1401129127b56f4f5af64c
Pierre Chalamet <pierre@chalamet.net>
2016-10-17 23:28:01 +0200

    ensure integration test use correct folder

--- src/FullBuild.Tests/Integration.fs
+++ src/FullBuild.Tests/Integration.fs
@@ -24,17 +24,22 @@ open System.Diagnostics
 
 let runFB (args : string) =
     printfn "==== running fullbuild %s" args
-    let runDir = TestContext.CurrentContext.TestDirectory + "/../../../" 
-    let fb = TestContext.CurrentContext.TestDirectory + "/../../../src/FullBuild/bin/fullbuild.exe"
-    let psi = ProcessStartInfo (FileName = fb, Arguments = args, UseShellExecute = false, WorkingDirectory = runDir, LoadUserProfile = true, CreateNoWindow = true, RedirectStandardOutput = true)
-    use proc = Process.Start (psi)
-    use stdout = proc.StandardOutput
-    while stdout.EndOfStream |> not do
-        let line = stdout.ReadLine()
-        printfn "> %s" line
+    let currFolder = TestContext.CurrentContext.TestDirectory
+    try
+        System.Environment.CurrentDirectory <- TestContext.CurrentContext.TestDirectory
+        let wsDir = Env.GetFolder Env.Folder.Workspace
+        let fb = wsDir |> IoHelpers.GetFile  "src/FullBuild/bin/fullbuild.exe"
+        let psi = ProcessStartInfo (FileName = fb.FullName, Arguments = args, UseShellExecute = false, WorkingDirectory = wsDir.FullName, LoadUserProfile = true, CreateNoWindow = true, RedirectStandardOutput = true)
+        use proc = Process.Start (psi)
+        use stdout = proc.StandardOutput
+        while stdout.EndOfStream |> not do
+            let line = stdout.ReadLine()
+            printfn "> %s" line
 
-    proc.WaitForExit()
-    if proc.ExitCode <> 0 then failwithf "Process failed with error %d" proc.ExitCode
+        proc.WaitForExit()
+        if proc.ExitCode <> 0 then failwithf "Process failed with error %d" proc.ExitCode
+    finally
+        System.Environment.CurrentDirectory <- currFolder
 
 [<Test>]
 let CheckSourceBuildIsSameAsBinaryBuild () = 
