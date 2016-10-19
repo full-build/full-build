29fc350396bb581c39394ceefd471dbfd5de906c
Pierre Chalamet <pierre@chalamet.net>
2016-10-19 22:38:42 +0200

    refactoring

--- src/FullBuild/Tools/Publishers.fs
+++ src/FullBuild/Tools/Publishers.fs
@@ -12,7 +12,7 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-module Core.Publishers
+module Tools.Publishers
 open IoHelpers
 open Env
 open System.IO
