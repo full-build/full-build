2e0ac0dbf32e6534ea2fac9ee0a7761100fef928
Pierre Chalamet <pierre.chalamet@availpro.com>
2016-10-23 19:05:18 +0200

    add logs

--- publish.cmd
+++ publish.cmd
@@ -4,7 +4,7 @@ setlocal
 set VERSION=%1
 set VERSTATUS=%2
 if [%VERSION%] == [] set VERSION=0.0.0
-echo publishing version %VERSION%
+echo publishing version '%VERSION%' with status '%VERSTATUS%'
 
 set HERE=%~dp0
 
@@ -15,8 +15,9 @@ goto :ok
 %HERE%\src\fullbuild\bin\fullbuild publish * || goto :ko
 robocopy %HERE%\apps\full-build %HERE%\refbin /MIR
 
-nuget pack -NoDefaultExcludes -NoPackageAnalysis -NonInteractive -OutputDirectory apps -Version %VERSION%%VERSTATUS% full-build.nuspec || goto :ko
-move apps\full-build.%VERSION%.nupkg apps\full-build.nupkg || goto :ko
+set NUGETVERSION=%VERSION%%VERSTATUS%
+nuget pack -NoDefaultExcludes -NoPackageAnalysis -NonInteractive -OutputDirectory apps -Version %NUGETVERSION% full-build.nuspec || goto :ko
+move apps\full-build.%NUGETVERSION%.nupkg apps\full-build.nupkg || goto :ko
 verify >nul
 goto :eof
 
