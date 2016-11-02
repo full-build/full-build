cc6a8c9958769cbabb04de08007673e074983104
Sergii Salata <sergii.salata@availpro.com>
2016-11-02 10:35:02 +0100

    register system extension on upgrade. add an icon (bad quality)

---
+++ src/FullBuild/OsHelpers.fs
@@ -0,0 +1,24 @@
+﻿module OsHelpers
+
+open Microsoft.Win32
+
+let RegisterSystemExtension (installFolder : System.IO.DirectoryInfo) =
+    let extension = IoHelpers.Extension.View |> IoHelpers.GetExtensionString |> sprintf ".%s"
+    let keyName = "fullbuild"
+    let description = "Fullbuild view file"
+    let openWith = System.IO.Path.Combine(installFolder.FullName, "fullbuild.exe")
+    let icon = System.IO.Path.Combine(installFolder.FullName, "favicon.ico")
+    
+    let baseKey = Registry.ClassesRoot.CreateSubKey(extension)
+    baseKey.SetValue("", keyName)
+
+    let openMethod = Registry.ClassesRoot.CreateSubKey(keyName)
+    openMethod.SetValue("", description)
+    openMethod.CreateSubKey("DefaultIcon").SetValue("",sprintf "\"%s\"" icon)
+    
+    let shell = openMethod.CreateSubKey("Shell")
+    shell.CreateSubKey("edit").CreateSubKey("command").SetValue("", sprintf "\"%s\" \"%%1\"" openWith);
+    shell.CreateSubKey("open").CreateSubKey("command").SetValue("", sprintf "\"%s\" \"%%1\"" openWith);
+    baseKey.Close()
+    openMethod.Close()
+    shell.Close()
