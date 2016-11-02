4b04bb7ada2124a1cb76f04665f3da604def867a
Sergii Salata <sergii.salata@availpro.com>
2016-11-02 23:25:32 +0100

    not for mono (for now)

--- src/FullBuild/OsHelpers.fs
+++ src/FullBuild/OsHelpers.fs
@@ -3,22 +3,23 @@
 open Microsoft.Win32
 
 let RegisterSystemExtension (installFolder : System.IO.DirectoryInfo) =
-    let extension = IoHelpers.Extension.View |> IoHelpers.GetExtensionString |> sprintf ".%s"
-    let keyName = "fullbuild"
-    let description = "Fullbuild view file"
-    let openWith = System.IO.Path.Combine(installFolder.FullName, "fullbuild.exe")
-    let icon = System.IO.Path.Combine(installFolder.FullName, "favicon.ico")
+    if Env.IsMono() |> not then
+        let extension = IoHelpers.Extension.View |> IoHelpers.GetExtensionString |> sprintf ".%s"
+        let keyName = "fullbuild"
+        let description = "Fullbuild view file"
+        let openWith = System.IO.Path.Combine(installFolder.FullName, "fullbuild.exe")
+        let icon = System.IO.Path.Combine(installFolder.FullName, "favicon.ico")
     
-    let baseKey = Registry.ClassesRoot.CreateSubKey(extension)
-    baseKey.SetValue("", keyName)
+        let baseKey = Registry.ClassesRoot.CreateSubKey(extension)
+        baseKey.SetValue("", keyName)
 
-    let openMethod = Registry.ClassesRoot.CreateSubKey(keyName)
-    openMethod.SetValue("", description)
-    openMethod.CreateSubKey("DefaultIcon").SetValue("",sprintf "\"%s\"" icon)
+        let openMethod = Registry.ClassesRoot.CreateSubKey(keyName)
+        openMethod.SetValue("", description)
+        openMethod.CreateSubKey("DefaultIcon").SetValue("",sprintf "\"%s\"" icon)
     
-    let shell = openMethod.CreateSubKey("Shell")
-    shell.CreateSubKey("edit").CreateSubKey("command").SetValue("", sprintf "\"%s\" \"%%1\"" openWith);
-    shell.CreateSubKey("open").CreateSubKey("command").SetValue("", sprintf "\"%s\" \"%%1\"" openWith);
-    baseKey.Close()
-    openMethod.Close()
-    shell.Close()
+        let shell = openMethod.CreateSubKey("Shell")
+        shell.CreateSubKey("edit").CreateSubKey("command").SetValue("", sprintf "\"%s\" \"%%1\"" openWith);
+        shell.CreateSubKey("open").CreateSubKey("command").SetValue("", sprintf "\"%s\" \"%%1\"" openWith);
+        baseKey.Close()
+        openMethod.Close()
+        shell.Close()
