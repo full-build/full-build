314002ec983fe05a4a5e9f8eeff8a38fcf4be413
Pierre Chalamet <pierre@chalamet.net>
2016-11-03 08:24:57 +0100

    cleanup

--- src/FullBuild/Configuration.fs
+++ src/FullBuild/Configuration.fs
@@ -69,10 +69,18 @@ let DeleteView (viewId : ViewId) =
     if DefaultView() = Some viewId && defaultFile.Exists then
         defaultFile.Delete()
 
-let SetDefaultView (viewId : ViewId) =
+let private setDefaultView (viewId : ViewId) =
     let vwFolder = Env.GetFolder Env.Folder.View
     let defaultFile = vwFolder |> IoHelpers.GetFile "default"
     System.IO.File.WriteAllText (defaultFile.FullName, viewId.toString)
+
+let SaveView (viewId : ViewId) view (isDefault : bool option) =
+    let viewFile = GetViewFile viewId.toString
+    ViewSerializer.Save viewFile view
+    match isDefault with
+    | None -> ()
+    | Some false -> if DefaultView () = Some viewId then DeleteDefaultView()
+    | Some true -> setDefaultView viewId
 
 let ViewExists viewName =
     let viewFile = GetViewFile viewName 
