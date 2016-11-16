5886215ea4c05d8c41bd7cc83525541be24fa6be
Pierre Chalamet <pierre.chalamet@availpro.com>
2016-11-16 11:35:44 +0100

    fix missing package

--- src/FullBuild/Graph.fs
+++ src/FullBuild/Graph.fs
@@ -19,7 +19,7 @@ open XmlHelpers
 
 #nowarn "0346" // GetHashCode missing
 
-[<RequireQualifiedAccess>] 
+[<RequireQualifiedAccess>]
 type PackageVersion =
     | PackageVersion of string
     | Unspecified
@@ -65,11 +65,15 @@ with
     member this.Name = this.Package.toString
 
     static member GetPackageDependencies (xnuspec : System.Xml.Linq.XDocument) =
-        xnuspec.Descendants()
-            |> Seq.filter (fun x -> x.Name.LocalName = "dependency" && (!> x.Attribute(NsNone + "exclude") : string) <> "Compile")
-            |> Seq.map (fun x -> !> x.Attribute(NsNone + "id") : string)
-            |> Seq.map Anthology.PackageId.from
-            |> set
+        let pkgsDir = Env.GetFolder Env.Folder.Package
+        let dependencies = xnuspec.Descendants()
+                            |> Seq.filter (fun x -> x.Name.LocalName = "dependency" && (!> x.Attribute(NsNone + "exclude") : string) <> "Compile")
+                            |> Seq.map (fun x -> !> x.Attribute(NsNone + "id") : string)
+                            |> Seq.map Anthology.PackageId.from
+                            |> Seq.filter (fun x -> let path = pkgsDir |> IoHelpers.GetSubDirectory (x.toString)
+                                                    path.Exists)
+                            |> set
+        dependencies
 
     static member GetFrameworkDependencies (xnuspec : System.Xml.Linq.XDocument) =
         xnuspec.Descendants()
@@ -86,7 +90,7 @@ with
         let xnuspec = System.Xml.Linq.XDocument.Load (nuspecFile.FullName)
         Package.GetPackageDependencies xnuspec |> Set.map (fun x -> { Graph = this.Graph
                                                                       Package = x })
-        
+
     member this.FxAssemblies : Assembly set =
         let pkgsDir = Env.GetFolder Env.Folder.Package
         let pkgDir = pkgsDir |> IoHelpers.GetSubDirectory (this.Package.toString)
@@ -96,7 +100,7 @@ with
 
 // =====================================================================================================
 
-and [<CustomEquality; CustomComparison>] Assembly = 
+and [<CustomEquality; CustomComparison>] Assembly =
     { Graph : Graph
       Assembly : Anthology.AssemblyId }
 with
@@ -111,7 +115,7 @@ with
 
 and [<CustomEquality; CustomComparison>] Application =
     { Graph : Graph
-      Application : Anthology.Application } 
+      Application : Anthology.Application }
 with
     override this.Equals(other : System.Object) = refEquals this other
 
@@ -129,7 +133,7 @@ with
         this.Application.Projects |> Set.map (fun x -> this.Graph.ProjectMap.[x])
 
     member this.Delete () =
-        let newAntho = { this.Graph.Anthology 
+        let newAntho = { this.Graph.Anthology
                          with Applications = this.Graph.Anthology.Applications |> Set.remove this.Application }
         Graph(newAntho)
 
@@ -146,7 +150,7 @@ with
 
     member this.Name : string = this.Repository.Name.toString
 
-    member this.Builder = 
+    member this.Builder =
         let buildableRepo = this.Graph.Anthology.Repositories |> Seq.tryFind (fun x -> x.Repository.Name = this.Repository.Name)
         match buildableRepo with
         | Some repo -> match repo.Builder with
@@ -209,12 +213,12 @@ with
         this.Graph.Anthology.Projects |> Set.filter (fun x -> x.ProjectReferences |> Set.contains projectId)
                                       |> Set.map (fun x -> this.Graph.ProjectMap.[x.ProjectId])
 
-    member this.ProjectFile = 
+    member this.ProjectFile =
         this.Project.RelativeProjectFile.toString
 
     member this.Output = this.Graph.AssemblyMap.[this.Project.Output]
 
-    member this.BinFile = 
+    member this.BinFile =
         let repo = this.Repository.Name
         let path = System.IO.Path.GetDirectoryName(this.ProjectFile)
         let ass = this.Output.Name
@@ -245,10 +249,10 @@ with
 
     member this.HasTests = this.Project.HasTests
 
-    member this.AssemblyReferences = 
+    member this.AssemblyReferences =
         this.Project.AssemblyReferences |> Set.map (fun x -> this.Graph.AssemblyMap.[x])
 
-    member this.PackageReferences = 
+    member this.PackageReferences =
         this.Project.PackageReferences |> Set.map (fun x -> this.Graph.PackageMap.[x])
 
     static member CollectProjects (collector : Project -> Project set) (projects : Project set) =
@@ -297,7 +301,7 @@ and [<Sealed>] Graph(anthology : Anthology.Anthology) =
         packageMap
 
     member this.AssemblyMap : System.Collections.Generic.IDictionary<Anthology.AssemblyId, Assembly> =
-        if assemblyMap |> isNull then 
+        if assemblyMap |> isNull then
             let outputAss = anthology.Projects |> Seq.map (fun x -> x.Output)
                                                |> Set
             assemblyMap <- anthology.Projects |> Set.map (fun x -> x.AssemblyReferences)
@@ -308,8 +312,8 @@ and [<Sealed>] Graph(anthology : Anthology.Anthology) =
         assemblyMap
 
     member this.RepositoryMap : System.Collections.Generic.IDictionary<Anthology.RepositoryId, Repository> =
-        if repositoryMap |> isNull then 
-            repositoryMap <- anthology.Repositories |> Seq.map (fun x -> x.Repository.Name, { Graph = this; Repository = x.Repository})                                 
+        if repositoryMap |> isNull then
+            repositoryMap <- anthology.Repositories |> Seq.map (fun x -> x.Repository.Name, { Graph = this; Repository = x.Repository})
                                                     |> dict
         repositoryMap
 
@@ -319,7 +323,7 @@ and [<Sealed>] Graph(anthology : Anthology.Anthology) =
                                                      |> dict
         applicationMap
 
-    member this.ProjectMap : System.Collections.Generic.IDictionary<Anthology.ProjectId, Project> = 
+    member this.ProjectMap : System.Collections.Generic.IDictionary<Anthology.ProjectId, Project> =
         if projectMap |> isNull then
             projectMap <- anthology.Projects |> Seq.map (fun x -> x.ProjectId, { Graph = this; Project = x } )
                                              |> dict
@@ -356,7 +360,7 @@ and [<Sealed>] Graph(anthology : Anthology.Anthology) =
         let app = { Anthology.Application.Name = Anthology.ApplicationId.from name
                     Anthology.Application.Publisher = pub
                     Anthology.Application.Projects = projectIds }
-        let newAntho = { anthology 
+        let newAntho = { anthology
                          with Applications = anthology.Applications |> Set.add app }
         Graph(newAntho)
 
