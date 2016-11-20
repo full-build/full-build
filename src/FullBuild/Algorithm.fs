56f9f16eb6b365b76c5ab56f3532ba3f16f19807
Pierre Chalamet <pierre@chalamet.net>
2016-11-20 14:25:07 +0100

    improve view creation performance

---
+++ src/FullBuild/Algorithm.fs
@@ -0,0 +1,18 @@
+﻿module Algorithm
+open Collections
+
+let Closure<'T when 'T : comparison> (seeds : 'T set) (iterDown : 'T -> 'T set) (iterUp : 'T -> 'T set) : 'T set =
+    let rec exploreNext (node : 'T) (next : 'T -> 'T set) (path : 'T list) (boundaries : 'T set) =
+        let nextNodes = next node
+        Set.fold (fun s n -> s + (explore n next path s)) boundaries nextNodes
+
+    and explore (node : 'T) (next : 'T -> 'T set) (path : 'T list) (boundaries : 'T set) =
+        let currPath = node :: path
+        if boundaries |> Set.contains node then
+            currPath |> set
+        else
+            exploreNext node next currPath boundaries
+
+    let refBoundaries = Set.fold (fun s t -> exploreNext t iterDown [t] s) seeds seeds
+    let refByBoundaries = Set.fold (fun s t -> exploreNext t iterUp [t] s) refBoundaries seeds
+    refByBoundaries
