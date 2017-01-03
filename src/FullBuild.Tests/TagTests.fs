5c713e0481ff13dd66d2fbe1c0b196339cc4ea5e
Pierre Chalamet <pierre.chalamet@availpro.com>
2017-01-03 09:55:03 +0100

    more tests

---
+++ src/FullBuild.Tests/TagTests.fs
@@ -0,0 +1,37 @@
+﻿//   Copyright 2014-2017 Pierre Chalamet
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+module TagTests
+
+open NUnit.Framework
+open FsUnit
+
+
+[<Test>]
+let parse_tag_incremental () =
+    let tagInfo = Tag.Parse "fullbuild-master-1.2.3-inc"
+    let expectedTagInfo = { Tag.TagInfo.Branch = "master"
+                            Tag.TagInfo.BuildNumber = "1.2.3"
+                            Tag.TagInfo.Incremental = true }
+
+    tagInfo |> should equal expectedTagInfo
+
+[<Test>]
+let parse_tag_full () =
+    let tagInfo = Tag.Parse "fullbuild-beta-4.5-full"
+    let expectedTagInfo = { Tag.TagInfo.Branch = "beta"
+                            Tag.TagInfo.BuildNumber = "4.5"
+                            Tag.TagInfo.Incremental = false }
+
+    tagInfo |> should equal expectedTagInfo
--- src/FullBuild/Tools/Tag.fs
+++ src/FullBuild/Tools/Tag.fs
@@ -7,15 +7,31 @@ type TagInfo =
       BuildNumber : string 
       Incremental : bool }
 
+let (|FullBuild|Unknown|) x =
+    match x with
+    | "fullbuild" -> FullBuild
+    | _ -> Unknown
+
+let (|Incremental|Full|Unknown|) x =
+    match x with
+    | "inc" -> Incremental
+    | "full" -> Full
+    | _ -> Unknown
+
 let Parse (tag : string) : TagInfo =
-    let items = tag.Split('-')
-    if (items.Length <> 4 || items.[0] <> "fullbuild") then failwithf "Unknown tag"
-    let branch = items.[1]
-    let buildNumber = items.[2]
-    let buildType = items.[3]
-    { TagInfo.Branch = branch
-      TagInfo.BuildNumber = buildNumber
-      TagInfo.Incremental = buildType = "incremental" }
+    let items = tag.Split('-') |> List.ofArray
+    match items with
+    | [FullBuild; branch; version; Incremental] -> { TagInfo.Branch = branch; TagInfo.BuildNumber = version; TagInfo.Incremental = true }
+    | [FullBuild; branch; version; Full] -> { TagInfo.Branch = branch; TagInfo.BuildNumber = version; TagInfo.Incremental = true }
+    | _ -> failwithf "Unknown tag"
+
+//    if (items.Length <> 4 || items.[0] <> "fullbuild" || (items.[3] <> "inc" && items.[3] <> "full")) then failwithf "Unknown tag"
+//    let branch = items.[1]
+//    let buildNumber = items.[2]
+//    let buildType = items.[3]
+//    { TagInfo.Branch = branch
+//      TagInfo.BuildNumber = buildNumber
+//      TagInfo.Incremental = buildType = "inc" }
 
 let Format (tagInfo : TagInfo) : string =
     let inc = tagInfo.Incremental ? ("incremental", "full")
