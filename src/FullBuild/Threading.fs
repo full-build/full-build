08ea68913b92eaa6d4cb0585f95287ea94b5bf28
Pierre Chalamet <pierre@chalamet.net>
2017-09-18 22:18:06 +0200

    clearer throttle impl

--- src/FullBuild/Threading.fs
+++ src/FullBuild/Threading.fs
@@ -18,14 +18,18 @@ open System
 
 // http://stackoverflow.com/questions/3739531/how-to-limit-the-number-of-threads-created-for-an-asynchronous-seq-map-operation
 let private throttle n fs =
-    seq { let n = new Threading.Semaphore(n, n)
-          for f in fs ->
-              async { let! ok = Async.AwaitWaitHandle(n)
-                      let! result = Async.Catch f
-                      n.Release() |> ignore
-                      return match result with
-                             | Choice1Of2 rslt -> rslt
-                             | Choice2Of2 exn  -> raise exn } }
+    let n = new Threading.Semaphore(n, n)
+    let throttleTask f = 
+        async {
+            let! ok = Async.AwaitWaitHandle(n)
+            let! result = Async.Catch f
+            n.Release() |> ignore
+            return match result with
+                   | Choice1Of2 rslt -> rslt
+                   | Choice2Of2 exn  -> raise exn 
+        } 
+
+    fs |> Seq.map throttleTask 
 
 
 let ParExec fn from =
