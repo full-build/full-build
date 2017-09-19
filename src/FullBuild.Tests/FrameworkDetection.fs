c4b587c0372270d3e8cf9dc75c0498379a66741e
ssalata <sergii.salata@availpro.com>
2017-09-19 17:17:06 +0200

    parse package dependencies from nuspec and remove only used assemblies from csproj during convert

---
+++ src/FullBuild.Tests/FrameworkDetection.fs
@@ -0,0 +1,15 @@
+﻿module FrameworkDetection
+
+open NUnit.Framework
+open FsUnit
+open Collections
+open Paket
+
+[<Test>]
+let Parse () =
+    FrameworkDetection.Extract(".NETFramework2.0") |> should equal (Some (DotNetFramework FrameworkVersion.V2))
+    FrameworkDetection.Extract(".NETFramework3.5") |> should equal (Some (DotNetFramework FrameworkVersion.V3_5))
+    FrameworkDetection.Extract(".NETFramework4.0") |> should equal (Some (DotNetFramework FrameworkVersion.V4_Client))
+    FrameworkDetection.Extract(".NETFramework4.5") |> should equal (Some (DotNetFramework FrameworkVersion.V4_5))
+    FrameworkDetection.Extract(".NETStandard1.3") |> should equal (Some (DotNetStandard DotNetStandardVersion.V1_3))
+
