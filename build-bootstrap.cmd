6f4c44e9a8c78bb22bbac2db6dc7f253d64169d7
Pierre Chalamet <pierre@chalamet.net>
2017-09-21 22:28:22 +0200

    hack bootstap

--- build-bootstrap.cmd
+++ build-bootstrap.cmd
@@ -6,7 +6,6 @@ set HERE=%~dp0
 robocopy bootstrap\bin %HERE%\.full-build\bin /E
 robocopy bootstrap\views .full-build\views /E
 robocopy bootstrap\projects .full-build\projects /E
-robocopy bootstrap\packages .full-build\packages /E
 copy bootstrap\bootstrap.sln %HERE%
 
 msbuild /t:Rebuild bootstrap.sln || goto :ko
--- update-bootstrap.cmd
+++ update-bootstrap.cmd
@@ -4,7 +4,6 @@ setlocal
 set HERE=%~dp0
 
 robocopy .full-build\projects bootstrap\projects *.targets /S
-robocopy .full-build\packages bootstrap\packages *.targets /S
 
 :ok
 exit /b 0
